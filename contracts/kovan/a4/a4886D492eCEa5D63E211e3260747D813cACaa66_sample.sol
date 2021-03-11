303c0827a3e7bd6b9382a77f05a7bdf70ae5b46e
auto/updateContractsEtherscanWebKovan <tintinweb@users.noreply.github.com>
2021-03-11 20:22:38 +0000

    auto - updateContractsEtherscanWebKovan (kovan) - autocommit

---
+++ contracts/kovan/a4/a4886D492eCEa5D63E211e3260747D813cACaa66_sample.sol
@@ -0,0 +1,16 @@
+/**
+ *Submitted for verification at Etherscan.io on 2021-03-11
+*/
+
+pragma solidity ^0.4.17;
+
+contract sample{
+    uint256 public testVar;
+    
+    function setVar(uint256 _var) external{
+        testVar = _var;
+    }
+    function Varplus1() external{
+        testVar = testVar * 3;
+    }
+}
