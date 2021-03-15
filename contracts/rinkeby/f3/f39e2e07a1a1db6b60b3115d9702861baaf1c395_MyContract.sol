310a8265d21f0e07866c0899261960dc1b1edd1e
auto/updateContractsEtherscanWebRinkeby <tintinweb@users.noreply.github.com>
2021-03-15 14:14:43 +0000

    auto - updateContractsEtherscanWebRinkeby (rinkeby) - autocommit

---
+++ contracts/rinkeby/f3/f39e2e07a1a1db6b60b3115d9702861baaf1c395_MyContract.sol
@@ -0,0 +1,12 @@
+/**
+ *Submitted for verification at Etherscan.io on 2021-03-15
+*/
+
+pragma solidity ^0.8.0;
+
+/**
+ * Simple "Hello World" smart contract on Solidity.
+ */
+contract MyContract {
+    string public str = "Hello World!";
+}
