c4d36d2a42322b06a75e8e13cd15292f87c21adf
auto/updateContractsEtherscanWebRopsten <tintinweb@users.noreply.github.com>
2021-03-17 23:05:40 +0000

    auto - updateContractsEtherscanWebRopsten (ropsten) - autocommit

---
+++ contracts/ropsten/6e/6e4F6A477d94082E3a867d716A8d8e9c2aD66AC6_Greeter.sol
@@ -0,0 +1,14 @@
+/**
+ *Submitted for verification at Etherscan.io on 2021-03-17
+*/
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract Greeter {
+
+  constructor() {
+  }
+
+}
---
+++ contracts/ropsten/f9/f971ec570538874f33cbc35c5156e9c3bc8cef66_BlackHole.sol
@@ -0,0 +1,7 @@
+/**
+ *Submitted for verification at Etherscan.io on 2021-03-17
+*/
+
+pragma solidity ^0.5.12;
+
+contract BlackHole {}
