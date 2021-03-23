d11b926c4b019e35fc7d5fec471c3a1dc5dcb538
auto/updateContractsEtherscanWebRopsten <tintinweb@users.noreply.github.com>
2021-03-23 23:05:50 +0000

    auto - updateContractsEtherscanWebRopsten (ropsten) - autocommit

---
+++ contracts/ropsten/fd/fdef644f1fdececace65e82b96a0de60106c7517_HelloWorld.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.7.3;
+
+contract HelloWorld {
+    string public message;
+
+    constructor(string memory initMessage) {
+        message = initMessage;
+    }
+
+    function update(string memory newMessage) public {
+        message = newMessage;
+    }
+}
+
+{
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "libraries": {}
+}
