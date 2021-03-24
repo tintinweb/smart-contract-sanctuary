aa37f1bd40c9917cfaca966153cb72bf645adea5
auto/updateContractsEtherscanWebKovan <tintinweb@users.noreply.github.com>
2021-03-24 20:10:57 +0000

    auto - updateContractsEtherscanWebKovan (kovan) - autocommit

---
+++ contracts/kovan/41/417D7FecAF650EaE51B02c8447A6f940a2ecAC9a_Storage.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.8.0;
+
+contract Storage {
+    address public owner;
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": true,
+    "runs": 200
+  },
+  "evmVersion": "istanbul",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
---
+++ contracts/kovan/88/888a6318a3eca017a1fbdcbb176ce2dcf08e52e8_hello.sol
@@ -0,0 +1,11 @@
+/**
+ *Submitted for verification at Etherscan.io on 2021-03-24
+*/
+
+pragma solidity ^0.6.2;
+
+contract hello{
+    constructor() public{
+        
+    }
+}
