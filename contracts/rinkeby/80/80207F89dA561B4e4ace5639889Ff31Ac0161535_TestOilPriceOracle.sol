8214389d50cfed77f360734412fced1f9b5f019f
auto/updateContractsEtherscanWebRinkeby <tintinweb@users.noreply.github.com>
2021-03-26 14:06:55 +0000

    auto - updateContractsEtherscanWebRinkeby (rinkeby) - autocommit

---
+++ contracts/rinkeby/80/80207F89dA561B4e4ace5639889Ff31Ac0161535_TestOilPriceOracle.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.8.0;
+
+contract TestOilPriceOracle {
+    function OilpricePerEth() public view returns (uint256) {
+        return 26;
+    }
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "evmVersion": "istanbul",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
---
+++ contracts/rinkeby/a2/a2F6E6029638cCb484A2ccb6414499aD3e825CaC_Greeter.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.7.0;
+
+
+
+contract Greeter {
+    string private greeting;
+
+    constructor(string memory _greeting) {
+
+        greeting = _greeting;
+    }
+
+    function greet() public view returns (string memory) {
+        return greeting;
+    }
+
+    function setGreeting(string memory _greeting) public {
+
+        greeting = _greeting;
+    }
+}
+
+{
+  "optimizer": {
+    "enabled": true,
+    "runs": 2000
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "metadata": {
+    "useLiteralContent": true
+  },
+  "libraries": {}
+}
