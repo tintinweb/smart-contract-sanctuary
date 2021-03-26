26ed8ad52e6daccdf737dbc535b19a9348d86500
auto/updateContractsEtherscanWebRinkeby <tintinweb@users.noreply.github.com>
2021-03-26 02:16:45 +0000

    auto - updateContractsEtherscanWebRinkeby (rinkeby) - autocommit

---
+++ contracts/rinkeby/9c/9cf9417be658309cd8c139282eea479eb50dbb51_CountByTwo.sol
@@ -0,0 +1,13 @@
+/**
+ *Submitted for verification at Etherscan.io on 2021-03-25
+*/
+
+contract CountByTwo {
+    uint256 count;
+    
+    
+    function countUp() public returns(uint256) {
+        count = count + 2;
+        return count;
+    }
+}
---
+++ contracts/rinkeby/d7/D73459F39A19ddE899D46A86Bcf29ee78Dc15AE9_Greeter.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.7.0;
+
+
+
+contract Greeter {
+    string private greeting;
+
+    constructor(string memory _greeting) {
+
+        greeting = _greeting;
+    }
+
+    function greet() public view returns (string memory) {
+        return greeting;
+    }
+
+    function setGreeting(string memory _greeting) public {
+
+        greeting = _greeting;
+    }
+}
+
+{
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "libraries": {}
+}
