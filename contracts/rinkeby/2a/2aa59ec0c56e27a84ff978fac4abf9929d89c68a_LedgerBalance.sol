e783a8471ce561d3080277c855ec7f70ce20d359
auto/updateContractsEtherscanWebRinkeby <tintinweb@users.noreply.github.com>
2021-03-27 02:25:05 +0000

    auto - updateContractsEtherscanWebRinkeby (rinkeby) - autocommit

---
+++ contracts/rinkeby/2a/2aa59ec0c56e27a84ff978fac4abf9929d89c68a_LedgerBalance.sol
@@ -0,0 +1,20 @@
+/**
+ *Submitted for verification at Etherscan.io on 2021-03-26
+*/
+
+pragma solidity ^0.5.0;
+
+contract LedgerBalance {
+   mapping(address => uint) public balances;
+
+   function updateBalance(uint newBalance) public {
+      balances[msg.sender] = newBalance;
+   }
+}
+contract Updater {
+   function updateBalance() public returns (uint) {
+      LedgerBalance ledgerBalance = new LedgerBalance();
+      ledgerBalance.updateBalance(10);
+      return ledgerBalance.balances(address(this));
+   }
+}
