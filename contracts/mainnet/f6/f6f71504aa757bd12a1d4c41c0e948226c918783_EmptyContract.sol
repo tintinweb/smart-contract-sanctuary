11dc8c95301c2fc191b03f20a1563a0095709f8d
auto/updateContractsEtherscanWeb <tintinweb@users.noreply.github.com>
2021-03-28 13:16:50 +0000

    auto - updateContractsEtherscanWeb - autocommit

---
+++ contracts/mainnet/f6/f6f71504aa757bd12a1d4c41c0e948226c918783_EmptyContract.sol
@@ -0,0 +1,22 @@
+contract EmptyContract {
+    fallback() external {
+        revert();
+    }
+}
+
+{
+  "optimizer": {
+    "enabled": true,
+    "runs": 99999
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "libraries": {}
+}
