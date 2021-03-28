b17576faccf1f860646f795222da648b2cb1ddd6
auto/updateContractsEtherscanWebRopsten <tintinweb@users.noreply.github.com>
2021-03-28 23:03:48 +0000

    auto - updateContractsEtherscanWebRopsten (ropsten) - autocommit

---
+++ contracts/ropsten/d5/D59fF117476B59A78DaFD8460A934C0E96185bdb_Cookie.sol
@@ -0,0 +1,54 @@
+pragma solidity ^0.8.3;
+
+contract Bakery {
+
+  // index of created contracts
+
+  Cookie[] public contracts;
+
+  // useful to know the row count in contracts index
+
+  function getContractCount() public view returns(uint contractCount)
+  {
+    return contracts.length;
+  }
+
+  // deploy a new contract
+
+  function newCookie() public returns(Cookie newContract)
+  {
+    Cookie c = new Cookie();
+    contracts.push(c);
+    return c;
+  }
+}
+
+
+contract Cookie {
+
+
+
+  function getFlavor() public returns (string memory flavor)
+  {
+    return "mmm ... chocolate chip";
+  }    
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "evmVersion": "istanbul",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
