43cf819bac5027e38dd64104ba8285dcda49ee73
auto/updateContractsEtherscanWebRopsten <tintinweb@users.noreply.github.com>
2021-03-29 23:04:33 +0000

    auto - updateContractsEtherscanWebRopsten (ropsten) - autocommit

---
+++ contracts/ropsten/51/51D1d2b0887f8dF9E1235B1250f97199C10f0d37_Suka.sol
@@ -0,0 +1,55 @@
+pragma solidity ^0.8.3;
+
+contract Suka {
+
+  Cuka[] public contra;
+    
+  function getContractCount() public view returns(uint contractCount)
+  {
+    return contra.length;
+  }
+
+
+  function newCookie(uint256 time) public returns(Cuka newContract)
+  {
+    Cuka c = new Cuka(time);
+    contra.push(c);
+    return c;
+  }
+}
+
+
+contract Cuka {
+    
+uint256 public heh;
+uint256 public lel;
+
+constructor(uint256 leel) { 
+        heh=block.timestamp;  
+        lel=leel;   
+} 
+    
+  function getFlavor() public pure returns (string memory flavor)
+  {
+    return "mmm ... chocolate chip";
+  }    
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "evmVersion": "istanbul",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
