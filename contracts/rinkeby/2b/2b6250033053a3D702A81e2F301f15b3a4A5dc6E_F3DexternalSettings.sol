44f6e678a2bccce4c31a1615fa7a19b12ded23f3
auto/updateContractsEtherscanWebRinkeby <tintinweb@users.noreply.github.com>
2021-03-29 14:07:10 +0000

    auto - updateContractsEtherscanWebRinkeby (rinkeby) - autocommit

---
+++ contracts/rinkeby/2b/2b6250033053a3D702A81e2F301f15b3a4A5dc6E_F3DexternalSettings.sol
@@ -0,0 +1,38 @@
+pragma solidity ^0.4.24;
+
+contract F3DexternalSettings {
+    function getLongExtra()
+        public
+        view
+        returns(uint256)
+    {
+        return (0); // length of the very first ICO 
+    }
+
+    function getLongGap()
+        public
+        view
+        returns(uint256)
+    {
+        return (24 hours); // length of ICO phase
+    }
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": true,
+    "runs": 100
+  },
+  "evmVersion": "byzantium",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
