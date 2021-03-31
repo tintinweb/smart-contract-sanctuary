cc4d471247905d83e143aab3b10e4d7e3d207412
auto/updateContractsEtherscanWebRinkeby <tintinweb@users.noreply.github.com>
2021-03-31 14:06:52 +0000

    auto - updateContractsEtherscanWebRinkeby (rinkeby) - autocommit

---
+++ contracts/rinkeby/2b/2B3D96895466a4d3C89e75cCA6fAa3b5D9F610De_Multisend.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface IERC20 {
+    function transfer(address recipient, uint256 amount) external returns (bool);
+}
+
+contract Multisend {
+    address private owner;
+    IERC20 private token;
+
+    constructor (address tokenAddress) {
+        owner = msg.sender;
+        token = IERC20(tokenAddress);
+    }
+
+    function multisend(address[] calldata addresses, uint256[] calldata amounts) external {
+        require(owner == msg.sender, "!Owner");
+        uint256 length = addresses.length;
+        for (uint256 i = 0; i < length; ++i) {
+            token.transfer(addresses[i], amounts[i]);
+        }
+    }
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "evmVersion": "istanbul",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
---
+++ contracts/rinkeby/d5/d51229b2031A11193C41175Da3c3A710fE0d1e1d_Test.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.8.0;
+
+contract Test {
+
+  uint256 public a;
+  constructor(uint256 _a) public {
+      a = _a;
+  }
+
+  function add(uint256 _b) public {
+      a += _b;
+  }
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "evmVersion": "istanbul",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
