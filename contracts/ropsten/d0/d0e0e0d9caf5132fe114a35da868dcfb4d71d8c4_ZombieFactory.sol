a2a7ed612130add36abd44dc9b64a426f73b8c58
auto/updateContractsEtherscanWebRopsten <tintinweb@users.noreply.github.com>
2021-04-01 23:14:48 +0000

    auto - updateContractsEtherscanWebRopsten (ropsten) - autocommit

---
+++ contracts/ropsten/d0/d0e0e0d9caf5132fe114a35da868dcfb4d71d8c4_ZombieFactory.sol
@@ -0,0 +1,14 @@
+/**
+ *Submitted for verification at Etherscan.io on 2021-04-01
+*/
+
+pragma solidity ^0.8.2;
+
+contract ZombieFactory {
+
+    event NewZombie(uint zombieId, string name, uint dna);
+
+  
+
+
+}
