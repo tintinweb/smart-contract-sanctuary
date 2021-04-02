80781615743660050f4b9a5713b855a392ab8cd6
auto/updateContractsEtherscanWebKovan <tintinweb@users.noreply.github.com>
2021-04-02 20:10:18 +0000

    auto - updateContractsEtherscanWebKovan (kovan) - autocommit

---
+++ contracts/kovan/ca/CaAa32C4055d311AEfc2467E2b11701491544Ece_HelloWorld.sol
@@ -0,0 +1,33 @@
+pragma solidity >=0.4.16 <0.9.0;
+
+contract HelloWorld {
+
+    string public message;
+
+    constructor(string memory initMessage) public {
+        message = initMessage;
+    }
+
+    function update(string memory newMessage) public {
+        message = newMessage;
+    }
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "evmVersion": "istanbul",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
