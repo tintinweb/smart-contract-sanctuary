a13f59f93e7665de57b1becdbaa0d5d16a7a1567
auto/updateContractsEtherscanWebRinkeby <tintinweb@users.noreply.github.com>
2021-04-03 14:05:29 +0000

    auto - updateContractsEtherscanWebRinkeby (rinkeby) - autocommit

---
+++ contracts/rinkeby/9f/9f8787899CCfC746EE8a3aE2a60185DDa7fd50C8_Support.sol
@@ -0,0 +1,37 @@
+pragma solidity ^0.5.0;
+
+contract Support {
+    function generateBytes(address add, uint256 id) public pure returns (bytes memory) {
+        bytes memory mesageByte = abi.encodePacked(add, id);
+        return mesageByte;
+    }
+
+    function generateBytesForString(string memory message) public pure returns (bytes memory) {
+        bytes memory mesageByte = abi.encodePacked(message);
+        return mesageByte;
+    }
+
+    function generateBytesForPermissioned(address contractAdd, uint256 id, address userWallet) public pure returns (bytes memory) {
+        bytes memory mesageByte = abi.encodePacked(contractAdd, id, userWallet);
+        return mesageByte;
+    }
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": true,
+    "runs": 200
+  },
+  "evmVersion": "istanbul",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
