4fb5bb83a6319b4317d00023c36ff0108cb46015
auto/updateContractsEtherscanWebKovan <tintinweb@users.noreply.github.com>
2021-04-04 20:06:49 +0000

    auto - updateContractsEtherscanWebKovan (kovan) - autocommit

---
+++ contracts/kovan/71/717f51d599b3a44fd8d7709bdc025c3706d055f3_kenchanisawesome.sol
@@ -0,0 +1,13 @@
+/**
+ *Submitted for verification at Etherscan.io on 2021-04-04
+*/
+
+pragma solidity ^0.6.0;
+
+contract kenchanisawesome {
+    string public message;
+    
+    constructor() public {
+        message = "ken chan is AWESOME";
+    }
+}
