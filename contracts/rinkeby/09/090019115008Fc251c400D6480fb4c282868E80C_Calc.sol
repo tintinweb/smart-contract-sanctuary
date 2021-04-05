8e98ad575c5b7bc3f305678e0c7f521af14f29f9
auto/updateContractsEtherscanWebRinkeby <tintinweb@users.noreply.github.com>
2021-04-05 02:25:39 +0000

    auto - updateContractsEtherscanWebRinkeby (rinkeby) - autocommit

---
+++ contracts/rinkeby/09/090019115008Fc251c400D6480fb4c282868E80C_Calc.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.7.3;
+
+contract Calc {
+    uint private num = 0;
+
+    event NumberSet(address _from, uint value);
+
+    function getNum() public view returns(uint) {
+        return num;
+    }
+
+    function setNum(uint _num) public {
+        num = _num;
+
+        emit NumberSet(msg.sender, _num);
+    }
+}
+
+{
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "libraries": {}
+}
