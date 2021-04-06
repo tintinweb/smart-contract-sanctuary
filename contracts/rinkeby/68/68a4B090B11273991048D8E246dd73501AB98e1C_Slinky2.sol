238aec24759db1e9e807476f61e4530db19fe946
auto/updateContractsEtherscanWebRinkeby <tintinweb@users.noreply.github.com>
2021-04-06 02:31:56 +0000

    auto - updateContractsEtherscanWebRinkeby (rinkeby) - autocommit

---
+++ contracts/rinkeby/68/68a4B090B11273991048D8E246dd73501AB98e1C_Slinky2.sol
@@ -0,0 +1,9 @@
+/**
+ *Submitted for verification at Etherscan.io on 2021-04-05
+*/
+
+contract Slinky2{
+    
+    string public hello = "Hello world";
+    
+}
---
+++ contracts/rinkeby/ba/bAc8D63A4e3E2ce3Ef7D7403A66edF3Ad209A042_Test.sol
@@ -0,0 +1,56 @@
+/*
+ * SPDX-License-Identifier:    GPL-3.0
+ */
+
+pragma solidity 0.6.8;
+pragma experimental ABIEncoderV2;
+
+
+abstract contract ERC1271 {
+   
+}
+
+contract Test {
+    
+    constructor(uint x1, uint x2) public {
+       
+    }
+
+    function _registerStandard(bytes4 _interfaceId) internal {
+        
+    }
+
+    function initialize() public {
+        uint x = 2828282;
+        _registerStandard(type(ERC1271).interfaceId); 
+    } 
+}
+
+
+contract Test2 {
+    address public base;
+
+    constructor() public {
+       base = address(new Test(10, 20));
+    }
+}
+
+{
+  "optimizer": {
+    "enabled": true,
+    "runs": 20000
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "metadata": {
+    "useLiteralContent": true
+  },
+  "libraries": {}
+}
