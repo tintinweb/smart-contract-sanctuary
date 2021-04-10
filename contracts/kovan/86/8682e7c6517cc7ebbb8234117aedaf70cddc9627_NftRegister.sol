dd4a7570de657a91a669308f86f6b77c8706d1b9
tintinweb <tintinweb@oststrom.com>
2021-04-10 12:45:52 +0200

    autocommit - shouts out to etherscan.io :* remember where you come from

---
+++ contracts/kovan/86/8682e7c6517cc7ebbb8234117aedaf70cddc9627_NftRegister.sol
@@ -0,0 +1,10 @@
+/**
+ *Submitted for verification at Etherscan.io on 2021-04-10
+*/
+
+pragma solidity 0.5.16;
+
+
+contract NftRegister {
+
+}
---
+++ contracts/ropsten/7a/7a08652924234afb0f83e5ab2e0ecc1bf6e0396d_sendw.sol
@@ -0,0 +1,23 @@
+/**
+ *Submitted for verification at Etherscan.io on 2021-04-09
+*/
+
+pragma solidity >=0.7.0;
+
+
+
+contract sendw {
+    
+  
+    function doEvent( address payable  _to) public {
+        
+        _to.transfer(0);
+
+       
+        
+    }
+    
+    
+    
+    
+}
