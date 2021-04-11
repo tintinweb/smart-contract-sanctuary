d7a06c0efb101ea045b50e4e2364c782d4645b1d
auto/updateContractsEtherscanWeb <auto/updateContractsEtherscanWeb>
2021-04-11 08:04:31 +0000

    auto - updateContractsEtherscanWeb - autocommit

---
+++ contracts/kovan/af/af589F7A115b14C3825937bBd8A59203c84BE9c5_A.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract A {
+    B public b;
+
+    constructor() public {
+        b = new B();
+    }
+}
+
+contract B {}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "evmVersion": "istanbul",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
