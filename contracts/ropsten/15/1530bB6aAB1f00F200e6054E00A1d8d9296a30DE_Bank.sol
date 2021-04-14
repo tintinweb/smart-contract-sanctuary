2a0bb11dbeed9591c7464a8f7cd4fdddcf5224c2
auto/updateContractsEtherscanWeb <auto/updateContractsEtherscanWeb>
2021-04-14 23:10:13 +0000

    auto - updateContractsEtherscanWeb - autocommit

---
+++ contracts/ropsten/15/1530bB6aAB1f00F200e6054E00A1d8d9296a30DE_Bank.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.7;
+
+contract Bank {
+
+    mapping(address => uint256) public balanceOf;   // balances, indexed by addresses
+
+    function deposit() public payable {
+        balanceOf[msg.sender] += msg.value;     // adjust the account's balance
+    }
+
+    function withdraw() public payable{
+        balanceOf[msg.sender] -= msg.value;
+        msg.sender.transfer(msg.value);
+    }
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": true,
+    "runs": 999999
+  },
+  "evmVersion": "istanbul",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
---
+++ contracts/ropsten/b8/b8bFc23553c825ba9bAd6aCBbC87BBA229ee734F_Bank.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.7;
+contract Bank {
+
+    receive() external payable { }
+    
+    function deposit() public payable {
+        payable(address(this)).transfer(msg.value);
+    }
+    
+    function withdraw(uint amount) public payable {
+        msg.sender.transfer(amount * 1 ether);
+    }
+
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": true,
+    "runs": 999999
+  },
+  "evmVersion": "istanbul",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
