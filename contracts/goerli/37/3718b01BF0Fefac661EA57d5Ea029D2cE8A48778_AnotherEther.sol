d0f8c33e71c24d8f153f026d2817227404e5c2f3
auto/updateContractsEtherscanWebGoerli <tintinweb@users.noreply.github.com>
2021-04-15 21:05:41 +0000

    auto - updateContractsEtherscanWebGoerli (goerli) - autocommit

---
+++ contracts/goerli/37/3718b01BF0Fefac661EA57d5Ea029D2cE8A48778_AnotherEther.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.3;
+
+import "./ERC20.sol";
+
+contract AnotherEther is ERC20 {
+    constructor() ERC20("AnotherEther", "AETH") {
+        _mint(msg.sender, 21000000);
+    }
+}
