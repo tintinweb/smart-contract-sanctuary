3484a63fb2c0235eb1a88429c740ee96e7a67e1b
auto/updateContractsEtherscanWebRopsten <tintinweb@users.noreply.github.com>
2021-04-16 11:08:17 +0000

    auto - updateContractsEtherscanWebRopsten (ropsten) - autocommit

---
+++ contracts/ropsten/5e/5e9F84355d4c5Ba6C8b04E83D06800061C4b5b5c_MockOracle.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.7.0;
+
+contract MockOracle {
+    mapping(string=>uint) public priceOf;
+    function setPrice(string memory symbol, uint price) public {
+        priceOf[symbol] = price;
+    }
+
+    function getPrice(string memory symbol) public view returns(uint) {
+        uint n = uint(keccak256(abi.encodePacked(block.timestamp, block.difficulty)))%20;
+        uint dire = uint(keccak256(abi.encodePacked(block.timestamp, block.difficulty)))%2;
+        uint p = 0;
+        if (dire == 0) {
+            p = priceOf[symbol]*(1e18+n*1e16)/1e18;
+        } else {
+            p = priceOf[symbol]*(1e18-n*1e16)/1e18;
+        }
+        return p;
+    }
+}
+
+{
+  "evmVersion": "istanbul",
+  "libraries": {},
+  "metadata": {
+    "bytecodeHash": "ipfs",
+    "useLiteralContent": true
+  },
+  "optimizer": {
+    "enabled": true,
+    "runs": 200
+  },
+  "remappings": [],
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
