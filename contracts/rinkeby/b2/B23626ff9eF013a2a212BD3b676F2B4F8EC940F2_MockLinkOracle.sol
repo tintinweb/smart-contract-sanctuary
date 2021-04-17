a517ede94ea7deda9a6edc7ff723fd731cceb349
auto/updateContractsEtherscanWebRinkeby <tintinweb@users.noreply.github.com>
2021-04-17 02:27:28 +0000

    auto - updateContractsEtherscanWebRinkeby (rinkeby) - autocommit

---
+++ contracts/rinkeby/b2/B23626ff9eF013a2a212BD3b676F2B4F8EC940F2_MockLinkOracle.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0;
+
+contract MockLinkOracle {
+    
+  function latestAnswer() public view returns (int256) {
+    return int256(57800e8);
+  }
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "evmVersion": "istanbul",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
