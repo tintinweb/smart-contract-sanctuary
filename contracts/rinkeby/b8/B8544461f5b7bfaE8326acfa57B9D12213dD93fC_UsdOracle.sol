29f54b77cb75438c404cead7fbf42819e5700e2c
auto/updateContractsEtherscanWebRinkeby <tintinweb@users.noreply.github.com>
2021-04-20 14:09:58 +0000

    auto - updateContractsEtherscanWebRinkeby (rinkeby) - autocommit

---
+++ contracts/rinkeby/b8/B8544461f5b7bfaE8326acfa57B9D12213dD93fC_UsdOracle.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity 0.6.11;
+
+/// @title UsdOracle is a constant price oracle feed that always returns 1 USD in USDC precision.
+contract UsdOracle {
+
+    int256 constant USD_PRICE = 1 * 10 ** 8;
+
+    /**
+        @dev Returns the constant USD price.
+     */
+    function getLatestPrice() public pure returns (int256) {
+        return USD_PRICE;
+    }
+}
+
+{
+  "optimizer": {
+    "enabled": true,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "libraries": {}
+}
