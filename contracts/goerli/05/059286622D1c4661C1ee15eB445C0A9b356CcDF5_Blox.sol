a7c7c70004a83032123f0bffdf5147794c8bfda6
auto/updateContractsEtherscanWebGoerli <tintinweb@users.noreply.github.com>
2021-04-20 21:08:03 +0000

    auto - updateContractsEtherscanWebGoerli (goerli) - autocommit

---
+++ contracts/goerli/05/059286622D1c4661C1ee15eB445C0A9b356CcDF5_Blox.sol
@@ -0,0 +1,38 @@
+// File: contracts/Blox.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+ 
+contract Blox {
+  uint256 private value;
+
+  // Emitted when the stored value changes
+  event ValueChanged(uint256 newValue);
+
+  // Stores a new value in the contract
+  function store(uint256 newValue) public {
+    value = newValue;
+    emit ValueChanged(newValue);
+  }
+
+  // Reads the last stored value
+  function retrieve() public view returns (uint256) {
+    return value;
+  }
+}
+
+{
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "libraries": {}
+}
