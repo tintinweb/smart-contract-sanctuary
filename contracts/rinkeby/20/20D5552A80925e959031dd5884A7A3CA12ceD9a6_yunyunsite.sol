d76c9c22732bb23e9435a53935ae8f87c8b97808
auto/updateContractsEtherscanWebRinkeby <tintinweb@users.noreply.github.com>
2021-04-21 14:08:39 +0000

    auto - updateContractsEtherscanWebRinkeby (rinkeby) - autocommit

---
+++ contracts/rinkeby/20/20D5552A80925e959031dd5884A7A3CA12ceD9a6_yunyunsite.sol
@@ -0,0 +1,54 @@
+pragma solidity ^0.4.26;
+
+contract yunyunsite{
+    struct yunyun{
+        int points;
+        uint signAt;
+    }
+    
+    event signRecords(address,uint);
+    mapping(address => yunyun) public yun;
+    
+    
+    // modifier checkSignAt(address _address){
+    //     require(now > yun[_address].signAt + 1 days , "一天只能簽到一次!");
+    //     _;
+    // }
+    
+    modifier checkPointEnough(int256 point){
+        require(yun[msg.sender].points >= point , "點數不夠啦!");
+        _;
+    }
+    
+    function sign() public {
+        yun[msg.sender].points += 1;
+        yun[msg.sender].signAt = now;
+        emit signRecords(msg.sender,now);
+    }
+    
+    function exchange(int256 point) public checkPointEnough(point){
+        yun[msg.sender].points -= point;
+    }
+    
+    
+    
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "evmVersion": "byzantium",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
