7fe5046705a6873eca3d42e298e807db77954954
auto/updateContractsEtherscanWebRinkeby <tintinweb@users.noreply.github.com>
2021-04-23 14:08:06 +0000

    auto - updateContractsEtherscanWebRinkeby (rinkeby) - autocommit

---
+++ contracts/rinkeby/3f/3Fbde8C14e4CCFDcc8a0Ae69E3F6621F91245B59_PascalTriangle.sol
@@ -0,0 +1,43 @@
+pragma solidity ^0.8.0;
+
+contract PascalTriangle {
+    uint256[] currRow;
+    uint256[] nextRow;
+
+    function newTriangle(uint256 size) public {
+        currRow.push(1);
+
+        for (uint256 r = 2; r <= size; r++) {
+            nextRow.push(1);
+
+            for (uint256 c = 1; c < r - 1; c++) {
+                nextRow.push(currRow[c - 1] + currRow[c]);
+            }
+            nextRow[r - 1] = 1;
+
+            uint256[] memory temp;
+            temp = currRow;
+            currRow = nextRow;
+            nextRow = temp;
+        }
+    }
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "evmVersion": "istanbul",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
