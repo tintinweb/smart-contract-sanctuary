7ad1117d8688e698a26740bed7dd82769d95b9ff
auto/updateContractsEtherscanWebRopsten <tintinweb@users.noreply.github.com>
2021-04-23 23:07:58 +0000

    auto - updateContractsEtherscanWebRopsten (ropsten) - autocommit

---
+++ contracts/ropsten/07/077009b6e648597a054db4ca6352983353a65c46_MockTruPriceOracle.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.10;
+
+interface ITruPriceOracle {
+    function usdToTru(uint256 amount) external view returns (uint256);
+
+    function truToUsd(uint256 amount) external view returns (uint256);
+}
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.10;
+
+import "ITruPriceOracle.sol";
+
+contract MockTruPriceOracle is ITruPriceOracle {
+    function usdToTru(uint256 amount) external override view returns (uint256) {
+        return (amount * 4) / 1e10;
+    }
+
+    function truToUsd(uint256 amount) external override view returns (uint256) {
+        return (amount * 1e10) / 4;
+    }
+}
+
+{
+  "optimizer": {
+    "enabled": true,
+    "runs": 20000
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
---
+++ contracts/ropsten/20/20fa73e9d6ce61dbb0240e57c68db8b1124b9378_MockCurveGauge.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.10;
+
+contract MockCurveGauge {
+    function minter() external pure returns (address) {
+        return address(0);
+    }
+}
+
+{
+  "optimizer": {
+    "enabled": true,
+    "runs": 20000
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
