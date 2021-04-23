f69304d9e93d84235d463ca8f211c7c8afdc7146
auto/updateContractsEtherscanWebRopsten <tintinweb@users.noreply.github.com>
2021-04-23 11:06:12 +0000

    auto - updateContractsEtherscanWebRopsten (ropsten) - autocommit

---
+++ contracts/ropsten/be/bef569DBD249e8C0587C67e67ffd771a92e405b4_TodoList.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.16;
+
+contract TodoList {
+    string[] private _list;
+    // Emitted when the storeda new item is added to the list
+    event ItemAdded(string item);
+    // Adds a new item in the list
+    function addItem(string memory newItem) public {
+        _list.push(newItem);
+        emit ItemAdded(newItem);
+    }
+    // Gets the item from the list according to index    
+    function getListItem(uint256 index) public view returns (string memory item) {
+        return _list[index];
+    }
+
+    function getListSize() public view returns (uint256 size) {
+        return _list.length;
+    }
+}
+
+{
+  "metadata": {
+    "useLiteralContent": true
+  },
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
