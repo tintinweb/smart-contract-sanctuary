d1b4d38043887e20d96cd7f09400271695e5fb2f
auto/updateContractsEtherscanWebRopsten <tintinweb@users.noreply.github.com>
2021-04-24 11:04:56 +0000

    auto - updateContractsEtherscanWebRopsten (ropsten) - autocommit

---
+++ contracts/ropsten/69/69b5acb144f4f6803d8daa408a4d37f4f779b809_Hello.sol
@@ -0,0 +1,14 @@
+/**
+ *Submitted for verification at Etherscan.io on 2021-04-24
+*/
+
+pragma solidity ^0.4.24;
+contract Hello {
+    string public name;
+        constructor() public {
+            name = "我是一個智能合約";
+        }
+        function setName(string _name) public {
+            name = _name;
+        }
+}
---
+++ contracts/ropsten/93/93b0F86fBa96c2f53b952B23930A292a1543F7c6_CtrSimple.sol
@@ -0,0 +1,37 @@
+pragma solidity ^0.5.14;
+
+contract CtrSimple {
+    uint public myUint = 10;
+    
+    event NewUint(uint indexed newUint);
+    
+    function setUint(uint _myUint) public {
+        myUint = _myUint;
+        emit NewUint(_myUint);
+    }
+    
+    function doubleUint() public {
+        myUint = 2 * myUint;
+        emit NewUint(myUint);
+    }
+    
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "evmVersion": "istanbul",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
