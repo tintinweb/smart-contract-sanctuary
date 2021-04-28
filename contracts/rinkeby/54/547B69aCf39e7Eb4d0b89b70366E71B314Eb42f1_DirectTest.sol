1be05f4e1e5404f6449cb417a595749fa57a560b
auto/updateContractsEtherscanWebRinkeby <tintinweb@users.noreply.github.com>
2021-04-28 14:09:04 +0000

    auto - updateContractsEtherscanWebRinkeby (rinkeby) - autocommit

---
+++ contracts/rinkeby/54/547B69aCf39e7Eb4d0b89b70366E71B314Eb42f1_DirectTest.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract DirectTest {
+  
+
+    constructor(){}
+
+    function testRangeFunctionForDirectSwap(uint tokenInValue,uint tokenOutValue) external pure returns(bool){
+        return (tokenInValue/tokenOutValue)+(tokenOutValue/tokenInValue)==1;
+    }
+
+  
+
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "evmVersion": "istanbul",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
