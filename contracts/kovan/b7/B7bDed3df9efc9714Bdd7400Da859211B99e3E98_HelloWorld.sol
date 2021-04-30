6375ee06f1267ca819094c25480e0931e437437c
auto/updateContractsEtherscanWeb <auto/updateContractsEtherscanWeb>
2021-04-30 20:12:11 +0000

    auto - updateContractsEtherscanWeb - autocommit

---
+++ contracts/kovan/b7/B7bDed3df9efc9714Bdd7400Da859211B99e3E98_HelloWorld.sol
@@ -0,0 +1,10 @@
+/**
+ *Submitted for verification at Etherscan.io on 2021-04-30
+*/
+
+pragma solidity ^0.6.6;
+contract HelloWorld {
+    function sayHello() public pure returns(string memory){
+        return("hello world");
+    }
+}
