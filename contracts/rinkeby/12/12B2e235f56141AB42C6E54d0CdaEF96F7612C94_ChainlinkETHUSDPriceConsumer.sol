69b7d3732359c88dd4c4bb0c42a0b55f66befeec
auto/updateContractsEtherscanWebRinkeby <tintinweb@users.noreply.github.com>
2021-04-30 14:20:33 +0000

    auto - updateContractsEtherscanWebRinkeby (rinkeby) - autocommit

---
+++ contracts/rinkeby/12/12B2e235f56141AB42C6E54d0CdaEF96F7612C94_ChainlinkETHUSDPriceConsumer.sol
@@ -0,0 +1,68 @@
+pragma solidity ^0.6.0;
+
+interface AggregatorV3Interface {
+  function decimals() external view returns (uint8);
+  
+  function latestRoundData()
+    external
+    view
+    returns (
+      uint80 roundId,
+      int256 answer,
+      uint256 startedAt,
+      uint256 updatedAt,
+      uint80 answeredInRound
+    );
+
+}
+
+pragma solidity ^0.6.0;
+
+import "./AggregatorV3Interface.sol";
+
+contract ChainlinkETHUSDPriceConsumer {
+
+    AggregatorV3Interface internal priceFeed;
+
+
+    constructor() public {
+        priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+    }
+
+    /**
+     * Returns the latest price
+     */
+    function getLatestPrice() public view returns (int) {
+        (
+            , 
+            int price,
+            ,
+            ,
+            
+        ) = priceFeed.latestRoundData();
+        return price;
+    }
+    
+    function getDecimals() public view returns (uint8) {
+        return priceFeed.decimals();
+    }
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": true,
+    "runs": 200
+  },
+  "evmVersion": "istanbul",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
---
+++ contracts/rinkeby/87/87f09ef8ee083577C2B5f3AAD6cc4789d443dF11_ChainlinkBNBUSDPriceConsumer.sol
@@ -0,0 +1,68 @@
+pragma solidity ^0.6.0;
+
+interface AggregatorV3Interface {
+  function decimals() external view returns (uint8);
+  
+  function latestRoundData()
+    external
+    view
+    returns (
+      uint80 roundId,
+      int256 answer,
+      uint256 startedAt,
+      uint256 updatedAt,
+      uint80 answeredInRound
+    );
+
+}
+
+pragma solidity ^0.6.0;
+
+import "./AggregatorV3Interface.sol";
+
+contract ChainlinkBNBUSDPriceConsumer {
+
+    AggregatorV3Interface internal priceFeed;
+
+
+    constructor() public {
+        priceFeed = AggregatorV3Interface(0xcf0f51ca2cDAecb464eeE4227f5295F2384F84ED);
+    }
+
+    /**
+     * Returns the latest price
+     */
+    function getLatestPrice() public view returns (int) {
+        (
+            , 
+            int price,
+            ,
+            ,
+            
+        ) = priceFeed.latestRoundData();
+        return price;
+    }
+    
+    function getDecimals() public view returns (uint8) {
+        return priceFeed.decimals();
+    }
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": true,
+    "runs": 200
+  },
+  "evmVersion": "istanbul",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
