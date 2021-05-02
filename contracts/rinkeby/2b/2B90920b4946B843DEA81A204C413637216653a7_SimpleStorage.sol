cb1ab7591dd3dec000f0d78ed766278b66386354
auto/updateContractsEtherscanWebRinkeby <tintinweb@users.noreply.github.com>
2021-05-02 14:17:28 +0000

    auto - updateContractsEtherscanWebRinkeby (rinkeby) - autocommit

---
+++ contracts/rinkeby/2b/2B90920b4946B843DEA81A204C413637216653a7_SimpleStorage.sol
@@ -0,0 +1,43 @@
+pragma solidity ^0.5.8;
+
+contract SimpleStorage {
+
+    uint storedData;
+
+    //event Change(string message, uint indexed newVal);
+    event Change(string message, uint newVal);
+
+    constructor (uint s) public {
+        storedData = s;
+    }
+
+    function set(uint x) public {
+        require(x < 1000, "Should be less than 1000");
+        storedData = x;
+        emit Change("set", x);
+    }
+
+    function get() public view returns (uint) {
+        return storedData;
+    }
+
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "evmVersion": "istanbul",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
