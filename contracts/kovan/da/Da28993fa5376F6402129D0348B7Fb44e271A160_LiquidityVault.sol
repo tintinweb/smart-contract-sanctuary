68cfd0f2b420ce12e8c13209ded48f3d9e852f40
auto/updateContractsEtherscanWebKovan <tintinweb@users.noreply.github.com>
2021-05-04 08:15:12 +0000

    auto - updateContractsEtherscanWebKovan (kovan) - autocommit

---
+++ contracts/kovan/da/Da28993fa5376F6402129D0348B7Fb44e271A160_LiquidityVault.sol
@@ -0,0 +1,13 @@
+/**
+ *Submitted for verification at Etherscan.io on 2021-05-04
+*/
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.4;
+
+
+
+// File: LiquidityVault.sol
+
+contract LiquidityVault {}
