128e0c486d43ff12a1cc4cc2affb0ea52edbaab4
auto/updateContractsEtherscanWebRinkeby <tintinweb@users.noreply.github.com>
2021-05-04 14:18:28 +0000

    auto - updateContractsEtherscanWebRinkeby (rinkeby) - autocommit

---
+++ contracts/rinkeby/2f/2F3A599933687c6968f15380Ba6F8E5104234f32_KryptoSign.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.0;
+
+contract KryptoSign {
+    
+    event DocumentPublished(uint256 id);
+
+    mapping (uint256 => string) documentMetadata;
+
+    uint256 latestDocumentId;
+
+    constructor () { 
+        latestDocumentId = 1;
+    }
+    
+    function publishDocument(string memory metadata) external {
+        documentMetadata[latestDocumentId] = metadata;
+        emit DocumentPublished(latestDocumentId);
+        latestDocumentId++;
+    }
+
+    function getDocument(uint256 documentId) external view returns (string memory) {
+        return documentMetadata[documentId];
+    }
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "evmVersion": "istanbul",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
