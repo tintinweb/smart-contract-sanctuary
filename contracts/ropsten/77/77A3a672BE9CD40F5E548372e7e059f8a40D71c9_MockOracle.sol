0b378ec07f6e72a15a5347443d94f872f6654c0a
auto/updateContractsEtherscanWebRopsten <tintinweb@users.noreply.github.com>
2021-05-04 11:10:02 +0000

    auto - updateContractsEtherscanWebRopsten (ropsten) - autocommit

---
+++ contracts/ropsten/77/77A3a672BE9CD40F5E548372e7e059f8a40D71c9_MockOracle.sol
@@ -0,0 +1,30 @@
+pragma solidity >=0.6.0 <0.8.0;
+
+interface IOracle {
+    function getValue() external view returns (uint256);
+}
+
+contract MockOracle is IOracle {
+    constructor() public {}
+    
+    function getValue() override external view returns (uint256) {
+        return uint(blockhash(block.number - 1));
+    }
+}
+
+{
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "libraries": {}
+}
