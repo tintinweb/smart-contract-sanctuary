70289fe63a70c6635b03e13960a7c330a1d98625
auto/updateContractsEtherscanWeb <auto/updateContractsEtherscanWeb>
2021-05-06 13:19:55 +0000

    auto - updateContractsEtherscanWeb - autocommit

---
+++ contracts/mainnet/f9/f94cc1707e49886f24f30a32f3b649277d386412_Migrations.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Migrations {
+  address public owner = msg.sender;
+  uint public last_completed_migration;
+
+  modifier restricted() {
+    require(
+      msg.sender == owner,
+      "This function is restricted to the contract's owner"
+    );
+    _;
+  }
+
+  function setCompleted(uint completed) external restricted {
+    last_completed_migration = completed;
+  }
+}
+
+{
+  "metadata": {
+    "useLiteralContent": true
+  },
+  "optimizer": {
+    "enabled": true,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
