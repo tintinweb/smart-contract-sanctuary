2318f934a278fdc8308443842e050b6e054d0137
auto/updateContractsEtherscanWebRinkeby <tintinweb@users.noreply.github.com>
2021-05-07 14:18:34 +0000

    auto - updateContractsEtherscanWebRinkeby (rinkeby) - autocommit

---
+++ contracts/rinkeby/c7/c79c2EAA9C6d5Ad6CC630B626C4E04C6451a8706_BlackHole.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.12;
+
+// This is a black hole contract address
+// no one can transfer MDX from the contract
+contract BlackHole {}
+
+{
+  "optimizer": {
+    "enabled": true,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "libraries": {}
+}
