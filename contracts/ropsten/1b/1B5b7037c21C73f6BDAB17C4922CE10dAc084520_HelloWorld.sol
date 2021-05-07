307ac1c49540dfc1368cc8e78d2cbfa1ade724b5
auto/updateContractsEtherscanWebRopsten <tintinweb@users.noreply.github.com>
2021-05-07 23:10:38 +0000

    auto - updateContractsEtherscanWebRopsten (ropsten) - autocommit

---
+++ contracts/ropsten/1b/1B5b7037c21C73f6BDAB17C4922CE10dAc084520_HelloWorld.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.5.16;
+contract HelloWorld {
+    function sayHello() public pure returns(string memory){
+        return("hello world");
+    }
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "evmVersion": "istanbul",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
---
+++ contracts/ropsten/bb/bB9D61039d4B9E5A423536e4582a2C6D8fF64592_MockOracle.sol
@@ -0,0 +1,38 @@
+pragma solidity ^0.6.0;
+
+import "../interfaces/IPriceFeed.sol";
+
+contract MockOracle is IPriceFeed {
+    uint public price;
+
+    function setPrice(uint _price) public {
+        price = _price;
+    }
+
+    function getPrice() external override view returns(uint) {
+        return price;
+    }
+}
+
+pragma solidity ^0.6.0;
+
+interface IPriceFeed {
+    function getPrice() external view returns(uint);
+}
+
+{
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "libraries": {}
+}
