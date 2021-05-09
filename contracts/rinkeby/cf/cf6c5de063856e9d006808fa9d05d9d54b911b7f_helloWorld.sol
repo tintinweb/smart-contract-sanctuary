27a081af71882f1549e1e7ebf3348b387ebb17e0
auto/updateContractsEtherscanWebRinkeby <tintinweb@users.noreply.github.com>
2021-05-09 02:43:10 +0000

    auto - updateContractsEtherscanWebRinkeby (rinkeby) - autocommit

---
+++ contracts/rinkeby/cf/cf6c5de063856e9d006808fa9d05d9d54b911b7f_helloWorld.sol
@@ -0,0 +1,11 @@
+/**
+ *Submitted for verification at Etherscan.io on 2021-05-08
+*/
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract helloWorld {
+    function renderHelloWorld() public pure returns (string memory) {
+        return "helloWorld";
+    }
+}
