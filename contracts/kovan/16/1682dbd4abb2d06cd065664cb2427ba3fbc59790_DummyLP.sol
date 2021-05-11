186cc8556ae89ef4c85f9a164d695ec9a38e9ca1
auto/updateContractsEtherscanWebKovan <tintinweb@users.noreply.github.com>
2021-05-11 20:15:43 +0000

    auto - updateContractsEtherscanWebKovan (kovan) - autocommit

---
+++ contracts/kovan/16/1682dbd4abb2d06cd065664cb2427ba3fbc59790_DummyLP.sol
@@ -0,0 +1,19 @@
+/**
+ *Submitted for verification at Etherscan.io on 2021-05-11
+*/
+
+pragma solidity ^0.7.0;
+
+
+contract DummyLP {
+
+
+    constructor() {
+        
+    }
+
+    function safeTransferFrom(address from, address to, uint256 amt) public returns(bool) {
+        return true;
+    }
+    
+}
