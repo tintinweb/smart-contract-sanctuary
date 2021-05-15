7452361d07ea1eb4ef815dad420b29b31f820553
auto/updateContractsEtherscanWebRopsten <tintinweb@users.noreply.github.com>
2021-05-15 23:16:35 +0000

    auto - updateContractsEtherscanWebRopsten (ropsten) - autocommit

---
+++ contracts/ropsten/a2/a26b4569f24bed6fe4b3e71267af40407e925c77_MyContract.sol
@@ -0,0 +1,13 @@
+/**
+ *Submitted for verification at Etherscan.io on 2021-05-15
+*/
+
+pragma solidity 0.8.4;
+
+contract MyContract {
+    uint256 val;
+
+    function getVal() external returns (uint256) {
+        return val;
+    }
+}
