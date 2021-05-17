c5e4c1b1f41f767b3849e0807c45d67860d1ad4b
auto/updateContractsEtherscanWeb <auto/updateContractsEtherscanWeb>
2021-05-17 08:11:57 +0000

    auto - updateContractsEtherscanWeb - autocommit

---
+++ contracts/kovan/18/18AbeEf74b5CEe20e47bfF8A7C5EF8c0a345e43A_RelayProxy.sol
@@ -0,0 +1,27 @@
+pragma solidity 0.6.9;
+
+contract RelayProxy {
+    uint256 public x =1;
+    function test() public  {
+        x+=1;
+    }
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": true,
+    "runs": 200
+  },
+  "evmVersion": "istanbul",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
