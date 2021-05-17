ea19ebc351a4777592b2c0519d7e105d4235061d
auto/updateContractsEtherscanWebKovan <tintinweb@users.noreply.github.com>
2021-05-17 20:17:17 +0000

    auto - updateContractsEtherscanWebKovan (kovan) - autocommit

---
+++ contracts/kovan/e2/E2C119C9b53AfE4B39a193a165C142f3c4D7Bb9b_Helper.sol
@@ -0,0 +1,36 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+
+contract Helper {
+
+  function stringToBytes32(string memory source) public pure returns (bytes32 result) {
+    bytes memory temp = bytes(source);
+    if (temp.length == 0) {
+        return 0x0;
+    }
+
+    assembly {
+      result := mload(add(source, 32))
+    }
+  }
+}
+
+{
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "metadata": {
+    "useLiteralContent": true
+  },
+  "libraries": {}
+}
