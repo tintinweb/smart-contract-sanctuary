dfbe17d7d9d9c716d027ab41670e189731477817
auto/updateContractsEtherscanWebRinkeby <tintinweb@users.noreply.github.com>
2021-05-18 14:21:05 +0000

    auto - updateContractsEtherscanWebRinkeby (rinkeby) - autocommit

---
+++ contracts/rinkeby/85/85b7cCdcB005D30b73FC23F35b463b92F1Ac072d_HelloWorld.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract HelloWorld{
+    string public name;
+
+    function setName(string memory _name) public {
+       name = _name; 
+    }
+
+    function getName() public view returns(string memory) {
+       return name; 
+    }
+}
+
+{
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "libraries": {}
+}
