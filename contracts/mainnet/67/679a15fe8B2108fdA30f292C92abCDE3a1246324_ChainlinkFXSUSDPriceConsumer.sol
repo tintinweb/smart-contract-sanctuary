093a4b42356ef6ff8806383a5583ec3556e9cb10
auto/updateContractsEtherscanWeb <auto/updateContractsEtherscanWeb>
2021-05-18 02:18:32 +0000

    auto - updateContractsEtherscanWeb - autocommit

---
+++ contracts/mainnet/67/679a15fe8B2108fdA30f292C92abCDE3a1246324_ChainlinkFXSUSDPriceConsumer.sol
@@ -0,0 +1,88 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.11;
+pragma experimental ABIEncoderV2;
+
+import "./AggregatorV3Interface.sol";
+
+contract ChainlinkFXSUSDPriceConsumer {
+
+    AggregatorV3Interface internal priceFeed;
+
+
+    constructor() public {
+        priceFeed = AggregatorV3Interface(0x6Ebc52C8C1089be9eB3945C4350B68B8E4C2233f);
+    }
+
+    /**
+     * Returns the latest price
+     */
+    function getLatestPrice() public view returns (int) {
+        (
+            , 
+            int price,
+            ,
+            ,
+            
+        ) = priceFeed.latestRoundData();
+        return price;
+    }
+
+    function getDecimals() public view returns (uint8) {
+        return priceFeed.decimals();
+    }
+}
+
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0;
+
+interface AggregatorV3Interface {
+
+  function decimals() external view returns (uint8);
+  function description() external view returns (string memory);
+  function version() external view returns (uint256);
+
+  // getRoundData and latestRoundData should both raise "No data present"
+  // if they do not have data to report, instead of returning unset values
+  // which could be misinterpreted as actual reported values.
+  function getRoundData(uint80 _roundId)
+    external
+    view
+    returns (
+      uint80 roundId,
+      int256 answer,
+      uint256 startedAt,
+      uint256 updatedAt,
+      uint80 answeredInRound
+    );
+  function latestRoundData()
+    external
+    view
+    returns (
+      uint80 roundId,
+      int256 answer,
+      uint256 startedAt,
+      uint256 updatedAt,
+      uint80 answeredInRound
+    );
+
+}
+
+{
+  "optimizer": {
+    "enabled": true,
+    "runs": 100000
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "metadata": {
+    "useLiteralContent": true
+  },
+  "libraries": {}
+}
