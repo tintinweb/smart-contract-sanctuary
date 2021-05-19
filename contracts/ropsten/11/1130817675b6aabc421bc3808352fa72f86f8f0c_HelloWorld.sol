02f9623d8bfd780b38ef13a582fd1a5dd197496c
auto/updateContractsEtherscanWebRopsten <tintinweb@users.noreply.github.com>
2021-05-19 23:17:05 +0000

    auto - updateContractsEtherscanWebRopsten (ropsten) - autocommit

---
+++ contracts/ropsten/11/1130817675b6aabc421bc3808352fa72f86f8f0c_HelloWorld.sol
@@ -0,0 +1,12 @@
+/**
+ *Submitted for verification at Etherscan.io on 2021-05-19
+*/
+
+//SPDX-License-Identifier: Unlicensed
+pragma solidity >=0.7.6;
+
+contract HelloWorld {
+    function myFirstHelloWorld() public pure returns (string memory) {
+        return 'Hello World';
+    }
+}
---
+++ contracts/ropsten/43/43afac4c22b1384cfd6a92f726852b48025682b9_HelloWorld.sol
@@ -0,0 +1,12 @@
+/**
+ *Submitted for verification at Etherscan.io on 2021-05-19
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <0.9.0;
+
+contract HelloWorld {
+    function helloWorld() external pure returns (string memory) {
+        return "Hello, World!";
+    }
+}
