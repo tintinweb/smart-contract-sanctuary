e66820fe562df819901b111c22133532c76e539a
auto/updateContractsEtherscanWebRinkeby <tintinweb@users.noreply.github.com>
2021-05-20 14:18:42 +0000

    auto - updateContractsEtherscanWebRinkeby (rinkeby) - autocommit

---
+++ contracts/rinkeby/fe/Fe291Ad6493Fe98Dae016f4BF0D2618f369CD12b_BuybackController.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.6.12;
+
+contract BuybackController {
+  uint256 public buybackAmount;
+
+  function setBuybackAmount(uint256 _buybackAmount) public {
+    buybackAmount = _buybackAmount;
+  }
+}
+
+{
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "libraries": {}
+}
