7f129571b24e7aa426a9820883d9ccb49866c9a6
auto/updateContractsEtherscanWebRopsten <tintinweb@users.noreply.github.com>
2021-05-21 11:13:39 +0000

    auto - updateContractsEtherscanWebRopsten (ropsten) - autocommit

---
+++ contracts/ropsten/6a/6a96c8ecb8b294249c3b912172d6e40b617d466e_Likelion_16.sol
@@ -0,0 +1,17 @@
+/**
+ *Submitted for verification at Etherscan.io on 2021-05-21
+*/
+
+//seo sangcheol
+
+pragma solidity 0.8.1;
+
+contract Likelion_16 {
+    
+    uint balance = 0;
+    
+    function returnbalance() public view returns(uint) {
+        return address(this).balance;
+    }
+
+}
