7772309e4e9a2b7e55dd5ee30c481c2924c30000
auto/updateContractsEtherscanWebRinkeby <tintinweb@users.noreply.github.com>
2021-05-22 14:16:38 +0000

    auto - updateContractsEtherscanWebRinkeby (rinkeby) - autocommit

---
+++ contracts/rinkeby/b4/b477eF6Fda4BE5b85fF376bA807d745b88bE8bAA_test.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: GPL-2.0
+pragma solidity ^0.8.4;
+
+contract test{
+    uint256 public _id;
+    constructor(uint256 id){
+        _id = id;
+    }
+}
+
+{
+  "evmVersion": "istanbul",
+  "libraries": {},
+  "metadata": {
+    "bytecodeHash": "ipfs",
+    "useLiteralContent": true
+  },
+  "optimizer": {
+    "enabled": true,
+    "runs": 200
+  },
+  "remappings": [],
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
