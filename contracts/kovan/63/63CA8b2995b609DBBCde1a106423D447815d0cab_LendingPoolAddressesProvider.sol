9a99aad933f6caf6342b474e95407c113de1e77e
auto/updateContractsEtherscanWeb <auto/updateContractsEtherscanWeb>
2021-05-22 08:11:01 +0000

    auto - updateContractsEtherscanWeb - autocommit

---
+++ contracts/kovan/63/63CA8b2995b609DBBCde1a106423D447815d0cab_LendingPoolAddressesProvider.sol
@@ -0,0 +1,39 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.7.6;
+
+contract LendingPoolAddressesProvider{
+
+    address lendingPool;
+
+    constructor (address _lendingPool){
+        lendingPool = _lendingPool;
+    }
+
+    function getLendingPool() public view returns(address){
+        return lendingPool;
+    }
+
+}
+
+{
+  "evmVersion": "istanbul",
+  "libraries": {},
+  "metadata": {
+    "bytecodeHash": "ipfs",
+    "useLiteralContent": true
+  },
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "remappings": [],
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
