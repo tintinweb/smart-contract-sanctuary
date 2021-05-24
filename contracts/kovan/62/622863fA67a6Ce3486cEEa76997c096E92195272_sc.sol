9503524336092a067078cea38cdab89c9c23b06f
auto/updateContractsEtherscanWeb <auto/updateContractsEtherscanWeb>
2021-05-24 08:11:35 +0000

    auto - updateContractsEtherscanWeb - autocommit

---
+++ contracts/kovan/62/622863fA67a6Ce3486cEEa76997c096E92195272_sc.sol
@@ -0,0 +1,10 @@
+/**
+ *Submitted for verification at Etherscan.io on 2021-05-24
+*/
+
+pragma solidity ^0.7.0;
+contract sc{
+     function echo() public returns(string memory){
+         return "sccc";
+     }
+}
