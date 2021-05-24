790d9c684bd23819ecdcb2961fcd731c36ead1c9
auto/updateContractsEtherscanWebRopsten <tintinweb@users.noreply.github.com>
2021-05-24 11:16:47 +0000

    auto - updateContractsEtherscanWebRopsten (ropsten) - autocommit

---
+++ contracts/ropsten/6b/6bbf679d03f028d1cedb5c1b2bf52ce0b86aeccf_Root.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.7.2;
+
+import "./Test1.sol";
+import "./Test2.sol";
+
+contract Root is Test1, Test2 {}
---
+++ contracts/ropsten/c3/C30a3210D82f01f291C0eCf18ec6b85180329e58_TestContract.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: agpl-3.0
+pragma solidity 0.7.6;
+pragma abicoder v2;
+
+
+contract TestContract {
+
+  struct ContractInfo1 {
+      uint256 value11;
+      uint256 value12;
+  }
+  struct ContractInfo2 {
+      ContractInfo1 info1;
+      uint256 value2;
+  }
+
+  ContractInfo2 contractInfo;
+
+  constructor() {}
+
+  function setData(ContractInfo2 calldata _newInfo) external {
+    contractInfo = _newInfo;
+  }
+
+  function getData() external view returns (ContractInfo2 memory info2) {
+    info2 = contractInfo;
+  }
+}
+
+{
+  "optimizer": {
+    "enabled": true,
+    "runs": 1000
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "libraries": {}
+}
