0ee68130a1753889a416d080dbe2688988ef4135
auto/updateContractsEtherscanWebRopsten <tintinweb@users.noreply.github.com>
2021-05-25 23:19:04 +0000

    auto - updateContractsEtherscanWebRopsten (ropsten) - autocommit

---
+++ contracts/ropsten/67/6703830CD0a25A0541aA263DD1AcA2abd1Bc0597_Migrations.sol
@@ -0,0 +1,44 @@
+pragma solidity >=0.4.25;
+
+contract Migrations {
+
+    address public owner;
+    uint public last_completed_migration;
+
+    modifier restricted() {
+        if (msg.sender == owner)
+            _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function setCompleted(uint completed) public restricted {
+        last_completed_migration = completed;
+    }
+
+    function upgrade(address newAddress) public restricted {
+        Migrations upgraded = Migrations(newAddress);
+        upgraded.setCompleted(last_completed_migration);
+    }
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "evmVersion": "istanbul",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
