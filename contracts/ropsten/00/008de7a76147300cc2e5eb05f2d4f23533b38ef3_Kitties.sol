a58741b248e0f63da384213f8867ca01ff1652ec
auto/updateContractsEtherscanWebRopsten <tintinweb@users.noreply.github.com>
2021-05-25 11:20:34 +0000

    auto - updateContractsEtherscanWebRopsten (ropsten) - autocommit

---
+++ contracts/ropsten/00/008de7a76147300cc2e5eb05f2d4f23533b38ef3_Kitties.sol
@@ -0,0 +1,12 @@
+/**
+ *Submitted for verification at Etherscan.io on 2021-05-25
+*/
+
+contract Kitties {
+    uint256 public mua = 123; 
+    function setMua(uint256 _newMua) public {
+        mua = _newMua;
+        
+    }
+
+}
---
+++ contracts/ropsten/eb/eb58c206827793061fd6fd7593d2ac238e94bf05_Dog.sol
@@ -0,0 +1,16 @@
+/**
+ *Submitted for verification at Etherscan.io on 2021-05-25
+*/
+
+pragma solidity ^0.5.16;
+
+
+
+contract Dog {
+    uint256 public mua = 123; 
+    function setMua(uint256 _newMua) public {
+        mua = _newMua;
+        
+    }
+
+}
