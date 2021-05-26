3121c2531f9e9b3c91a8ce817bec887ed8fe9237
auto/updateContractsEtherscanWeb <auto/updateContractsEtherscanWeb>
2021-05-26 08:32:37 +0000

    auto - updateContractsEtherscanWeb - autocommit

---
+++ contracts/kovan/f7/f7dfa2c0c8f7e083b5f86819fcdac6c094b173f4_hello.sol
@@ -0,0 +1,17 @@
+/**
+ *Submitted for verification at Etherscan.io on 2021-05-25
+*/
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+contract hello {
+
+    string public greeting;
+
+    constructor() public {
+        greeting = "hello";
+        
+    }
+}
