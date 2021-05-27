74f9a87be13741945cfe59adb7834529908dceb7
auto/updateContractsEtherscanWebRinkeby <tintinweb@users.noreply.github.com>
2021-05-27 03:46:34 +0000

    auto - updateContractsEtherscanWebRinkeby (rinkeby) - autocommit

---
+++ contracts/rinkeby/35/356cf84366f8F0f2e1a5Ffe1AC9c166933FA68ab_BoxV2.sol
@@ -0,0 +1,45 @@
+// contracts/BoxV2.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract BoxV2 {
+    uint256 private value;
+
+    // Emitted when the stored value changes
+    event ValueChanged(uint256 newValue);
+
+    // Stores a new value in the contract
+    function store(uint256 newValue) public {
+        value = newValue;
+        emit ValueChanged(newValue);
+    }
+
+    // Reads the last stored value
+    function retrieve() public view returns (uint256) {
+        return value;
+    }
+
+    function increment() public {
+        value = value + 1;
+        emit ValueChanged(value);
+    }
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "evmVersion": "byzantium",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
