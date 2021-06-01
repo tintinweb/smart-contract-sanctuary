d91f4e6dcb5d2c44d4387e22083080f22f7f030c
auto/updateContractsEtherscanWebRinkeby <tintinweb@users.noreply.github.com>
2021-06-01 14:52:16 +0000

    auto - updateContractsEtherscanWebRinkeby (rinkeby) - autocommit

---
+++ contracts/rinkeby/9d/9da91285BEF51f1863e8B2a24b34673f0368325A_DERO.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.5.16;
+
+contract DERO {
+    int private count = 0;
+    function incrementCounter() public {
+        count += 1;
+    }
+    function decrementCounter() public {
+        count -= 1;
+    }
+    function getCount() public view returns (int) {
+        return count;
+    }
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "evmVersion": "istanbul",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
---
+++ contracts/rinkeby/b9/b901F09aB3591102e128b64abC4a58E6247a50A8_DERO.sol
@@ -0,0 +1,32 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.7.3;
+
+contract DERO {
+    int private count = 0;
+    function incrementCounter() public {
+        count += 1;
+    }
+    function decrementCounter() public {
+        count -= 1;
+    }
+    function getCount() public view returns (int) {
+        return count;
+    }
+}
+
+{
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "libraries": {}
+}
