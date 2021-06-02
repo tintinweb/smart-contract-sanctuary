212cde935372b04446c7c74cbd410c62e528f86b
auto/updateContractsEtherscanWeb <auto/updateContractsEtherscanWeb>
2021-06-02 09:42:00 +0000

    auto - updateContractsEtherscanWeb - autocommit

---
+++ contracts/kovan/38/3814B80Df228055EA043F14219c5b70F40a7Bf14_Upkeep.sol
@@ -0,0 +1,35 @@
+contract Upkeep {
+  bool public shouldPerformUpkeep;
+  bytes public bytesToSend;
+  bytes public receivedBytes;
+  function setShouldPerformUpkeep(bool _should) public {
+    shouldPerformUpkeep = _should;
+  }
+  function setBytesToSend(bytes memory _bytes) public {
+    bytesToSend = _bytes;
+  }
+  function checkUpkeep(bytes calldata data) external returns (bool, bytes memory) {
+    return (shouldPerformUpkeep, bytesToSend);
+  }
+  function performUpkeep(bytes calldata data) external {
+    shouldPerformUpkeep = false;
+    receivedBytes = data;
+  }
+}
+
+{
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "libraries": {}
+}
