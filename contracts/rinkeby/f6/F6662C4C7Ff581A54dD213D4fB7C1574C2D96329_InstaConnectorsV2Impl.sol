5d25f92e8241f51c9d5bcfc6042cd728e3c33857
auto/updateContractsEtherscanWebRinkeby <tintinweb@users.noreply.github.com>
2021-06-03 14:40:22 +0000

    auto - updateContractsEtherscanWebRinkeby (rinkeby) - autocommit

---
+++ contracts/rinkeby/f6/F6662C4C7Ff581A54dD213D4fB7C1574C2D96329_InstaConnectorsV2Impl.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.7.0;
+
+contract InstaConnectorsV2Impl  {
+
+}
+
+{
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "metadata": {
+    "useLiteralContent": true
+  },
+  "libraries": {}
+}
