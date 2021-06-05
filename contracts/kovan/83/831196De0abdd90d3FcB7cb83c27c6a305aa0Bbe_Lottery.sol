dc553420e5801db1f8bbed161c0d560ba3911968
auto/updateContractsEtherscanWeb <auto/updateContractsEtherscanWeb>
2021-06-05 08:39:18 +0000

    auto - updateContractsEtherscanWeb - autocommit

---
+++ contracts/kovan/83/831196De0abdd90d3FcB7cb83c27c6a305aa0Bbe_Lottery.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.7.3;
+
+contract Lottery {
+  address public manager;
+  address[] public players;
+
+  constructor() {
+    manager = msg.sender;
+  }
+
+  function enter() public payable {
+    require(msg.value > .01 ether, 'requires payment');
+    players.push(msg.sender);
+  }
+}
+
+{
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "libraries": {}
+}
---
+++ contracts/kovan/d7/D7139Dd25b7ee197Fc2bE9E599475423Fd431123_Lottery.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.7.3;
+
+contract Lottery {
+  address public manager;
+
+  constructor() {
+    manager = msg.sender;
+  }
+}
+
+{
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "libraries": {}
+}
