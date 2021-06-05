cada19dd52322d21b7b2e9f77c4db805dc1468f0
auto/updateContractsEtherscanWebRinkeby <tintinweb@users.noreply.github.com>
2021-06-05 03:44:32 +0000

    auto - updateContractsEtherscanWebRinkeby (rinkeby) - autocommit

---
+++ contracts/rinkeby/7c/7ceb2b0ae9841fb435ecbd6390b85b3c197f95c7_NowContract.sol
@@ -0,0 +1,34 @@
+//SPDX-License-Identifier: Unlicensed
+pragma solidity ^0.7.5;
+
+contract NowContract {
+    uint32 public nowValue = 0;
+
+    constructor () {
+        computeNow();
+    }
+
+    function computeNow() public {
+        nowValue = uint32(block.timestamp);
+    }
+}
+
+{
+  "optimizer": {
+    "enabled": true,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "metadata": {
+    "useLiteralContent": true
+  },
+  "libraries": {}
+}
