29a539fdcb2aa6ad90ab68d7bb25a6f1335a03f1
auto/updateContractsEtherscanWeb <auto/updateContractsEtherscanWeb>
2021-06-06 14:05:10 +0000

    auto - updateContractsEtherscanWeb - autocommit

---
+++ contracts/mainnet/00/0025336944da9e6c4554f350d1e133b107845fd3_PublicData.sol
@@ -0,0 +1,11 @@
+/**
+ *Submitted for verification at Etherscan.io on 2021-06-06
+*/
+
+pragma solidity 0.4.25;
+
+contract PublicData {
+    uint public health = 100;
+    uint internal mana = 50;
+    string private secret = "foo";
+}
