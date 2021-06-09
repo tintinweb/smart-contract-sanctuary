d952e721b3b2c21e46db2bd56eee9a490ff80c05
auto/updateContractsEtherscanWeb <auto/updateContractsEtherscanWeb>
2021-06-09 13:33:44 +0000

    auto - updateContractsEtherscanWeb - autocommit

---
+++ contracts/mainnet/11/11b27e5c800e160e2540ab195f0d5db7340756da_TransferHelper.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity >=0.6.2;
+
+contract TransferHelper {
+  function multicall (bytes calldata src) external {
+    assembly {
+      let ptr := src.offset
+      let end := add(ptr, src.length)
+      let to := calldataload(ptr)
+
+      ptr := add(ptr, 32)
+
+      for {} lt(ptr, end) {} {
+        let inSize := byte(callvalue(), calldataload(ptr))
+        ptr := add(ptr, 1)
+        calldatacopy(callvalue(), ptr, inSize)
+        ptr := add(ptr, inSize)
+
+        let success := call(gas(), to, callvalue(), callvalue(), inSize, callvalue(), callvalue())
+        if iszero(success) {
+          returndatacopy(callvalue(), callvalue(), returndatasize())
+          revert(callvalue(), returndatasize())
+        }
+      }
+    }
+  }
+}
+
+{
+  "evmVersion": "berlin",
+  "libraries": {},
+  "metadata": {
+    "bytecodeHash": "none"
+  },
+  "optimizer": {
+    "details": {
+      "constantOptimizer": true,
+      "cse": true,
+      "deduplicate": true,
+      "jumpdestRemover": true,
+      "orderLiterals": false,
+      "peephole": true,
+      "yul": false
+    },
+    "runs": 256
+  },
+  "remappings": [],
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
