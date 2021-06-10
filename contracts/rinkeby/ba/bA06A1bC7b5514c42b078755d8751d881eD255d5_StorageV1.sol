1782d4ec902316e32e9a559e3656a5b4112d9dfa
auto/updateContractsEtherscanWebRinkeby <tintinweb@users.noreply.github.com>
2021-06-10 02:51:39 +0000

    auto - updateContractsEtherscanWebRinkeby (rinkeby) - autocommit

---
+++ contracts/rinkeby/ba/bA06A1bC7b5514c42b078755d8751d881eD255d5_StorageV1.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.7.0;
+
+contract StorageV1 {
+
+    uint256 private _value;
+
+    event ValueUpdated(uint256 value);
+    
+    function set(uint256 value) external {
+        _value = value;
+        emit ValueUpdated(_value);
+    }
+
+    function get() external view returns(uint256) {
+        return _value;
+    }
+}
+
+{
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "libraries": {}
+}
