a53ccda1f85e39ac1fe42f666ec40abcecb31401
auto/updateContractsEtherscanWebRinkeby <tintinweb@users.noreply.github.com>
2021-06-10 14:22:49 +0000

    auto - updateContractsEtherscanWebRinkeby (rinkeby) - autocommit

---
+++ contracts/rinkeby/33/33eB5925409b3a40bF6D854Bf982E9B8e5740bCe_MPHMinterLegacy.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.3;
+
+/**
+    @title Dummy MPHMinter that doesn't mint anything. For legacy support.
+*/
+contract MPHMinterLegacy {
+    function mintDepositorReward(
+        address, /*to*/
+        uint256, /*depositAmount*/
+        uint256, /*depositPeriodInSeconds*/
+        uint256 /*interestAmount*/
+    ) external pure returns (uint256) {
+        return 0;
+    }
+
+    function takeBackDepositorReward(
+        address, /*from*/
+        uint256, /*mintMPHAmount*/
+        bool /*early*/
+    ) external pure returns (uint256) {
+        return 0;
+    }
+
+    function mintFunderReward(
+        address, /*to*/
+        uint256, /*depositAmount*/
+        uint256, /*fundingCreationTimestamp*/
+        uint256, /*maturationTimestamp*/
+        uint256, /*interestPayoutAmount*/
+        bool /*early*/
+    ) external pure returns (uint256) {
+        return 0;
+    }
+}
+
+{
+  "evmVersion": "istanbul",
+  "libraries": {},
+  "metadata": {
+    "bytecodeHash": "ipfs",
+    "useLiteralContent": true
+  },
+  "optimizer": {
+    "enabled": true,
+    "runs": 200
+  },
+  "remappings": [],
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
