29d4e1186127c84804560e3902ba660b6e4fab0e
auto/updateContractsEtherscanWeb <auto/updateContractsEtherscanWeb>
2021-06-10 23:21:54 +0000

    auto - updateContractsEtherscanWeb - autocommit

---
+++ contracts/ropsten/19/191C76353BBaD755797c06c6e6dB00F970fA1ed4_SayHi.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.8.0;
+
+contract SayHi {
+    function sayHi() external pure returns(string memory) {
+        return "Hi";
+    }
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "evmVersion": "istanbul",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
