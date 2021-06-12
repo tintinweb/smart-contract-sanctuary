9873de58538aacb3946d6fa2b765c8c34ea23657
auto/updateContractsEtherscanWebRinkeby <tintinweb@users.noreply.github.com>
2021-06-12 14:21:31 +0000

    auto - updateContractsEtherscanWebRinkeby (rinkeby) - autocommit

---
+++ contracts/rinkeby/10/10872c05C0C3cEAeE45fDcA8cEb0Aa9E50474aF3_Bank.sol
@@ -0,0 +1,40 @@
+pragma solidity 0.5.16;
+
+contract Bank{
+    int bal;
+
+    constructor() public{
+        bal = 1;
+    }
+
+    function getBalance() view public returns(int) {
+        return bal;
+    }
+
+    function withdraw(int amt) public {
+        bal = bal - amt;
+    }
+
+    function deposit(int amt) public {
+        bal = bal + amt;
+    }
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "evmVersion": "istanbul",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
