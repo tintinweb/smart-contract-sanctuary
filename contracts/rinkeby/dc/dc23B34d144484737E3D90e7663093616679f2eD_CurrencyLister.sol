5b516dc2d5cc19deffcb97b459eb9f82f44be920
auto/updateContractsEtherscanWebRinkeby <tintinweb@users.noreply.github.com>
2021-06-14 14:18:57 +0000

    auto - updateContractsEtherscanWebRinkeby (rinkeby) - autocommit

---
+++ contracts/rinkeby/dc/dc23B34d144484737E3D90e7663093616679f2eD_CurrencyLister.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.8.0;
+pragma experimental ABIEncoderV2;
+
+// SPDX-License-Identifier: GPL-3.0-only
+struct TokenOptions {
+        // Whether or not the token implements the ERC777 standard.
+        bool isERC777;
+        // Whether or not the token charges transfer fees
+        bool hasTransferFee;
+}
+
+contract CurrencyLister {
+    function listCurrency(address token, TokenOptions memory options) public {
+    }
+}
+
+{
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "libraries": {}
+}
