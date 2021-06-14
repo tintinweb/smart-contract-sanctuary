1e8d05292ab03678c90ae7927c257fc68f5ad09c
auto/updateContractsEtherscanWebRopsten <tintinweb@users.noreply.github.com>
2021-06-14 11:09:58 +0000

    auto - updateContractsEtherscanWebRopsten (ropsten) - autocommit

---
+++ contracts/ropsten/4c/4c9cF0AE19994f16F9Cd67370dBC1dD691e73E22_ERC20.sol
@@ -0,0 +1,35 @@
+pragma solidity 0.8.0;
+
+contract ERC20 {
+  string public name;
+  string public symbol;
+  uint256 public totalSupply;
+  uint8 public decimals = 18;
+
+  mapping(address => uint256) public balances;
+
+  constructor(string memory _name, string memory _symbol, uint256 _totalSupply) public {
+    name = _name;
+    symbol = _symbol;
+    totalSupply = _totalSupply;
+
+    balances[msg.sender] = _totalSupply;
+  }
+}
+
+{
+  "optimizer": {
+    "enabled": true,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "libraries": {}
+}
---
+++ contracts/ropsten/b1/b1131CB2b78c1Cd3c537FAE4f0eA38dedc9977FB_Test.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.0;
+
+contract Test {
+	constructor () {
+
+	}
+
+	function description() public view returns (string memory) {
+		return 'TEST';
+	}
+}
+
+{
+  "optimizer": {
+    "enabled": true,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "libraries": {}
+}
