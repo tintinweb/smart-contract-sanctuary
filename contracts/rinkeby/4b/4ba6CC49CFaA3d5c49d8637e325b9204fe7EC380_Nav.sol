ef89a37355c6aaffe43f5df9378254e0edf4d8e4
auto/updateContractsEtherscanWebRinkeby <tintinweb@users.noreply.github.com>
2021-06-16 14:20:20 +0000

    auto - updateContractsEtherscanWebRinkeby (rinkeby) - autocommit

---
+++ contracts/rinkeby/4b/4ba6CC49CFaA3d5c49d8637e325b9204fe7EC380_Nav.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+contract Nav {
+    uint256 public value = 42;
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "evmVersion": "istanbul",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
