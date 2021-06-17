8a385a8f5d2747458c484e71aa9a1d538dc3d990
auto/updateContractsEtherscanWebGoerli <tintinweb@users.noreply.github.com>
2021-06-17 09:17:07 +0000

    auto - updateContractsEtherscanWebGoerli (goerli) - autocommit

---
+++ contracts/goerli/c3/c3699B06267376D233b85B15e6bcc1B9F42809d7_GreeterL1.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+
+contract GreeterL1 {
+  string greeting;
+  // constructor(string memory _greeting) {
+  //   // console.log("Deploying a Greeter with greeting:", _greeting);
+  //   greeting = _greeting;
+  // }
+
+  // constructor() {
+  //   greeting = 'Hello, World';
+  // }
+
+  function greet() public view returns (string memory) {
+    return greeting;
+  }
+
+  function setGreeting(string memory _greeting) public {
+    greeting = _greeting;
+  }
+}
+
+{
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "metadata": {
+    "useLiteralContent": true
+  }
+}
