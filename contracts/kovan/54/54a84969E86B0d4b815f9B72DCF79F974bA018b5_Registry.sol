d30ae62be2a9c124e75d5b3b32717e1573e1c0ec
auto/updateContractsEtherscanWeb <auto/updateContractsEtherscanWeb>
2021-06-18 20:11:27 +0000

    auto - updateContractsEtherscanWeb - autocommit

---
+++ contracts/kovan/54/54a84969E86B0d4b815f9B72DCF79F974bA018b5_Registry.sol
@@ -0,0 +1,42 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+
+contract Registry {
+  
+  mapping (address => string) public names;
+  mapping (string => address) public owners;
+
+  event Registered(address indexed who, string name);
+
+  function register(string memory name) external {
+    require(owners[name] == address(0), "That name is already taken");
+    address owner = msg.sender;
+    owners[name] = owner;
+    names[owner] = name;
+    emit Registered(owner, name);
+  }
+}
+
+{
+  "evmVersion": "istanbul",
+  "libraries": {},
+  "metadata": {
+    "bytecodeHash": "ipfs",
+    "useLiteralContent": true
+  },
+  "optimizer": {
+    "enabled": true,
+    "runs": 200
+  },
+  "remappings": [],
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
