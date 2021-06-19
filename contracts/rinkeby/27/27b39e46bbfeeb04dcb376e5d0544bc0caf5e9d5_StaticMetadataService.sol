1c90fee0d94831d606744a163b60f868ae9d6d3d
auto/updateContractsEtherscanWebRinkeby <tintinweb@users.noreply.github.com>
2021-06-19 02:46:36 +0000

    auto - updateContractsEtherscanWebRinkeby (rinkeby) - autocommit

---
+++ contracts/rinkeby/27/27b39e46bbfeeb04dcb376e5d0544bc0caf5e9d5_StaticMetadataService.sol
@@ -0,0 +1,30 @@
+pragma solidity >=0.8.4;
+
+contract StaticMetadataService {
+    string private _uri;
+
+    constructor(string memory _metaDataUri) {
+        _uri = _metaDataUri;
+    }
+
+    function uri(uint256) public view returns (string memory) {
+        return _uri;
+    }
+}
+
+{
+  "optimizer": {
+    "enabled": true,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "libraries": {}
+}
---
+++ contracts/rinkeby/7e/7e71fd91849FC170Ca523370C074B65A2AC87906_SupportBridge.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: BSD-3-Clause
+pragma solidity >=0.7.0 <0.9.0;
+
+contract SupportBridge {
+    function generateMessage(string memory _account, uint256 _amount, uint256 _chainId, uint256 _nonce, string memory _txHash, string memory _contractAddress) public pure returns(bytes memory) {
+        return abi.encodePacked(_account, _amount, _chainId, _nonce, _txHash, _contractAddress);
+    }
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": true,
+    "runs": 200
+  },
+  "evmVersion": "istanbul",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
