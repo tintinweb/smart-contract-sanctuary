480005c1954a903bb65d04d33cde37b356d5619c
auto/updateContractsEtherscanWeb <auto/updateContractsEtherscanWeb>
2021-06-23 20:10:09 +0000

    auto - updateContractsEtherscanWeb - autocommit

---
+++ contracts/kovan/b9/b978138917bbf7Bc5901c4FeDE402Dce6C3C6eaB_MyContract.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MyContract {
+    uint amount;
+
+    constructor (uint _startAmount) {
+        amount = _startAmount;
+    }
+
+    function getAmount() public view returns(uint) {
+        return amount;
+    }
+
+    function setAmount(uint _amount) public {
+        amount = _amount;
+    }
+}
+
+{
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "libraries": {}
+}
---
+++ contracts/kovan/cd/cd2eBc4d2bdb038ac42d7737007A05141b68F65b_Baseball.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.5;
+
+contract Baseball {
+  uint public number;
+
+  constructor(uint _initNumber) {
+    number = _initNumber;
+  }
+
+  function updateNumber(uint _newNumber) external {
+    number = _newNumber;
+  }
+}
+
+{
+  "evmVersion": "berlin",
+  "libraries": {},
+  "metadata": {
+    "bytecodeHash": "ipfs",
+    "useLiteralContent": true
+  },
+  "optimizer": {
+    "enabled": true,
+    "runs": 200
+  },
+  "remappings": [],
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
