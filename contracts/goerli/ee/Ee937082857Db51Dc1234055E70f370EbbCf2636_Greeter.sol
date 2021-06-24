314a2a3ff9e35e458d388e75399d6872e4a751ca
auto/updateContractsEtherscanWebGoerli <tintinweb@users.noreply.github.com>
2021-06-24 09:10:19 +0000

    auto - updateContractsEtherscanWebGoerli (goerli) - autocommit

---
+++ contracts/goerli/ee/Ee937082857Db51Dc1234055E70f370EbbCf2636_Greeter.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.4;
+
+error GreeterError();
+
+contract Greeter {
+    string public greeting;
+
+    constructor(string memory _greeting) {
+        greeting = _greeting;
+    }
+
+    function greet() public view returns (string memory) {
+        return greeting;
+    }
+
+    function setGreeting(string memory _greeting) public {
+        greeting = _greeting;
+    }
+
+    function throwError() external pure {
+        revert GreeterError();
+    }
+}
+
+{
+  "metadata": {
+    "bytecodeHash": "none",
+    "useLiteralContent": true
+  },
+  "optimizer": {
+    "enabled": true,
+    "runs": 800
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "libraries": {}
+}
