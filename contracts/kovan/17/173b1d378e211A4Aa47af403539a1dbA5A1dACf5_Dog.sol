878dbb8103c925b1b0a4d0135cb08793cb7ecff4
auto/updateContractsEtherscanWeb <auto/updateContractsEtherscanWeb>
2021-06-25 20:11:22 +0000

    auto - updateContractsEtherscanWeb - autocommit

---
+++ contracts/kovan/17/173b1d378e211A4Aa47af403539a1dbA5A1dACf5_Dog.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.6.12;
+
+contract Dog {
+    address payable public _charityWalletAddress;
+    address payable public _marketingWalletAddress;
+    address payable public _devWalletAddress;
+
+    constructor(
+        address payable charityWalletAddress,
+        address payable marketingWalletAddress,
+        address payable devWalletAddress
+    ) public {
+        _charityWalletAddress = charityWalletAddress;
+        _marketingWalletAddress = marketingWalletAddress;
+        _devWalletAddress = devWalletAddress;
+    }
+}
+
+{
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "libraries": {}
+}
