5efd5e2e89fa90783c4e42ff96beaf848327a57d
auto/updateContractsEtherscanWebRopsten <tintinweb@users.noreply.github.com>
2021-06-26 11:10:41 +0000

    auto - updateContractsEtherscanWebRopsten (ropsten) - autocommit

---
+++ contracts/ropsten/9a/9a2460392982e2a2506286dFA1d5c055925E59F6_HelloWorld.sol
@@ -0,0 +1,32 @@
+pragma solidity 0.5.3;
+
+contract HelloWorld {
+    string private message = "hello world";
+
+    function getMessage() public view returns(string memory) {
+        return message;
+    }
+
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": true,
+    "runs": 999
+  },
+  "evmVersion": "byzantium",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
