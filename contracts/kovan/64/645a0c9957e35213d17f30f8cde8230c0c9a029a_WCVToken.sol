c72068b3964ced96e8bcd5548b15fdad9a0826db
auto/updateContractsEtherscanWeb <auto/updateContractsEtherscanWeb>
2021-06-28 20:11:08 +0000

    auto - updateContractsEtherscanWeb - autocommit

---
+++ contracts/kovan/64/645a0c9957e35213d17f30f8cde8230c0c9a029a_WCVToken.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./ERC20.sol";
+
+contract WCVToken is ERC20{
+
+    constructor(uint256 initialSupply) ERC20("EduToken","WCS") {
+        _mint(msg.sender, initialSupply);
+    }
+
+}
