0b9f54e3eb7004a677dda8b384771af57a777262
auto/updateContractsEtherscanWebKovan <tintinweb@users.noreply.github.com>
2021-06-28 08:14:36 +0000

    auto - updateContractsEtherscanWebKovan (kovan) - autocommit

---
+++ contracts/kovan/ca/cab96161c6Ae99431eC76C272ba132E4A6D6B94C_IdempotentUpkeep.sol
@@ -0,0 +1,37 @@
+contract IdempotentUpkeep {
+  bool public shouldPerformUpkeep;
+  bytes public bytesToSend;
+  bytes public receivedBytes;
+  function setShouldPerformUpkeep(bool _should) public {
+    shouldPerformUpkeep = _should;
+  }
+  function setBytesToSend(bytes memory _bytes) public {
+    bytesToSend = _bytes;
+  }
+  function checkUpkeep(bytes calldata data) external returns (bool, bytes memory) {
+    return (shouldPerformUpkeep, bytesToSend);
+  }
+  function performUpkeep(bytes calldata data) external {
+    receivedBytes = data;
+  }
+  function getLastBytesSent() external returns (bytes memory) {
+    return bytesToSend;
+  }
+}
+
+{
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "libraries": {}
+}
