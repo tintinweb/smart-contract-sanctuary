c03363f1c9e9a3a17cc2a0d3953fe55bed48c06c
auto/updateContractsEtherscanWebRinkeby <tintinweb@users.noreply.github.com>
2021-06-28 02:34:15 +0000

    auto - updateContractsEtherscanWebRinkeby (rinkeby) - autocommit

---
+++ contracts/rinkeby/72/7232c65bFd6BD4db583feadC174926E9F38E1607_ClaudioTest.sol
@@ -0,0 +1,29 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.7.1;
+
+contract ClaudioTest {
+    function test(uint256 num1, uint256 num2)
+        public
+        view
+        returns (uint256 batatinha)
+    {
+        batatinha = num1 + num2;
+    }
+}
+
+{
+  "optimizer": {
+    "enabled": true,
+    "runs": 1000
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "libraries": {}
+}
