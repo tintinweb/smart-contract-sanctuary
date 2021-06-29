87a63aebb6018812d8e0103b55bb6a7af94c6b77
auto/updateContractsEtherscanWebGoerli <tintinweb@users.noreply.github.com>
2021-06-29 21:15:52 +0000

    auto - updateContractsEtherscanWebGoerli (goerli) - autocommit

---
+++ contracts/goerli/75/75011d285832cebc5f721AC0316fB680514d3Fda_AccountCreator.sol
@@ -0,0 +1,55 @@
+// contracts/AccountCreator.sol
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity 0.8.6;
+
+contract AccountCreator{
+
+    uint256 constant startAddress = 1000;
+    uint256 constant amountToSend = 1;
+    uint256 accountsCreated;
+    address lastCreatedAddress;
+
+    constructor() payable{
+
+    }
+
+    function create(uint256 accountNumber) public payable returns(bool){
+        for(uint256 i = 0; i < accountNumber; i++){
+            address addr = address(uint160(startAddress + accountsCreated + 1));
+            address payable recipient = payable(addr);
+            recipient.transfer(amountToSend);
+            accountsCreated++;
+            lastCreatedAddress = addr;
+        }
+        return true;
+    }
+
+    function getLastCreatedAddress() public view returns (address){
+      return lastCreatedAddress;
+    }
+
+    function getAccountsCreated() public view returns (uint256) {
+        return accountsCreated;
+    }
+
+    receive() external payable {}
+
+    fallback() external payable {}
+}
+
+{
+  "optimizer": {
+    "enabled": true,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "libraries": {}
+}
