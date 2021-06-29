58549b2f3ca4c1fb43cafe78e09f3866743b72da
auto/updateContractsEtherscanWeb <auto/updateContractsEtherscanWeb>
2021-06-29 20:10:38 +0000

    auto - updateContractsEtherscanWeb - autocommit

---
+++ contracts/kovan/3c/3c22872CEb714F0Fa982923A4467423624B0Ff84_Whitelist.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.8.5;
+
+contract Whitelist {
+    mapping(address => bool) public addresses;
+
+    function join() public {
+        addresses[msg.sender] = true;
+    }
+
+    function isAddressWhitelisted(address address_) public view returns (bool) {
+        return addresses[address_];
+    }
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "evmVersion": "berlin",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
