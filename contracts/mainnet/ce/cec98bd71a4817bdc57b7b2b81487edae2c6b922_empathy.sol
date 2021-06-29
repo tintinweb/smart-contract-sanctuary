f102990909c80417b6a3208e1bde6dc0375a3412
auto/updateContractsEtherscanWeb <tintinweb@users.noreply.github.com>
2021-06-29 13:27:45 +0000

    auto - updateContractsEtherscanWeb - autocommit

---
+++ contracts/mainnet/ce/cec98bd71a4817bdc57b7b2b81487edae2c6b922_empathy.sol
@@ -0,0 +1,10 @@
+/**
+ *Submitted for verification at Etherscan.io on 2021-06-29
+*/
+
+pragma solidity ^0.4.22;
+contract empathy {
+ function getUrl () public pure returns (string) {
+   return 'https://empathy.tw';
+ }
+}
