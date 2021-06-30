870506e691a3fd67c4a987852307c127168932ce
auto/updateContractsEtherscanWebRinkeby <tintinweb@users.noreply.github.com>
2021-06-30 02:35:25 +0000

    auto - updateContractsEtherscanWebRinkeby (rinkeby) - autocommit

---
+++ contracts/rinkeby/3d/3Dd7335Ee100Dc8B4f51531Cc45228A18BFd7347_RentalRecords.sol
@@ -0,0 +1,51 @@
+//SPDX-License-Identifier: MIT
+//Author: Jeff Prestes
+
+pragma solidity 0.8.6;
+
+contract RentalRecords {
+
+    struct Rent {
+        string locator;
+        string renter;
+        string addressHome;
+        uint rentalValue;
+    }
+
+    address public owner;
+
+    Rent[] public rentals;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function registerRental(
+        string memory paramLocator,
+        string memory paramRenter,
+        string memory paramAddressHome,
+        uint paramRentalValue
+    ) external returns (bool) {
+        require(msg.sender == owner, "Only the owner can register a rental contract");
+        Rent memory newRentalRecord = Rent(paramLocator, paramRenter, paramAddressHome, paramRentalValue);
+        rentals.push(newRentalRecord);
+        return true;
+    }
+    
+}
+
+{
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
