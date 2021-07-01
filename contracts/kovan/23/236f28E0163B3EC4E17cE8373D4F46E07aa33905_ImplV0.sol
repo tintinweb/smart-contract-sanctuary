d8412e932da9f20e75fed06cfb1a77090e05e8fc
auto/updateContractsEtherscanWebKovan <tintinweb@users.noreply.github.com>
2021-07-01 20:16:00 +0000

    auto - updateContractsEtherscanWebKovan (kovan) - autocommit

---
+++ contracts/kovan/23/236f28E0163B3EC4E17cE8373D4F46E07aa33905_ImplV0.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.7.3;
+
+contract ImplV0 {
+    uint256 public someVar = 111;
+
+    function setVar(uint256 _newValue) public {
+        someVar = _newValue;
+    }
+}
+
+{
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "libraries": {}
+}
