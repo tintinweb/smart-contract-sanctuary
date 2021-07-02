5e53bc8e963876daa460926198feeebe75d21220
auto/updateContractsEtherscanWeb <auto/updateContractsEtherscanWeb>
2021-07-02 20:11:20 +0000

    auto - updateContractsEtherscanWeb - autocommit

---
+++ contracts/kovan/a6/A631dE328f5500A4F7C5F95d22171CE74a78B870_Test.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.6.12;
+
+contract Test{
+  uint public constant VALUE = 999;
+}
+
+{
+  "optimizer": {
+    "enabled": true,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "libraries": {}
+}
