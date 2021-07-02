f2a08332e657719b0c151569a7c46303d176ac48
auto/updateContractsEtherscanWeb <auto/updateContractsEtherscanWeb>
2021-07-02 13:23:25 +0000

    auto - updateContractsEtherscanWeb - autocommit

---
+++ contracts/mainnet/cd/cd352280fd32665c8bf7f8784bfd1ea49d687324_AAA.sol
@@ -0,0 +1,39 @@
+pragma solidity ^0.4.24;
+
+library CCC {
+  function addCCC(uint256 a, uint256 b) pure returns (uint256) {
+    uint256 c = a - b;
+    return c;
+  }
+}
+
+contract AAA {
+    function aa() constant returns (uint256) {
+        uint256 x = 50;
+        return CCC.addCCC(50, x);
+    }
+}
+
+{
+  "optimizer": {
+    "enabled": false,
+    "runs": 0
+  },
+  "metadata": {
+    "bytecodeHash": "none"
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "libraries": {
+    "CCC.sol": {
+      "CCC": "0x7560aF28d77e1B0a452bD3D7D4dc96FF86815379"
+    }
+  }
+}
