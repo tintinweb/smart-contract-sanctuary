c41326dd5036a1e81313d4a6de7904bde1f39d6a
minion <minion>
2021-07-07 10:13:40 +0000

    with 💕

---
+++ contracts/mainnet/be/BeCF6e9224F235872916829f1B85d57aD97F57ef_MigrateETH.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+interface iOVM_L1ETHGateway {
+    function donateETH()
+    external
+    payable;
+}
+
+contract MigrateETH {
+    function migrateEth(address _to) public {
+        require(msg.sender == 0x9BA6e03D8B90dE867373Db8cF1A58d2F7F006b3A);
+        uint256 balance = address(this).balance;
+        iOVM_L1ETHGateway(_to).donateETH{value:balance}();
+    }
+}
+
+{
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
