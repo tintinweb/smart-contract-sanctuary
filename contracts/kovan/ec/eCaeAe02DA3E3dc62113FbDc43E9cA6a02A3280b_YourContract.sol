995598546df471f74e4a98dad91dc23a41cb7018
auto/updateContractsEtherscanWebKovan <tintinweb@users.noreply.github.com>
2021-07-08 20:13:36 +0000

    auto - updateContractsEtherscanWebKovan (kovan) - autocommit

---
+++ contracts/kovan/ec/eCaeAe02DA3E3dc62113FbDc43E9cA6a02A3280b_YourContract.sol
@@ -0,0 +1,45 @@
+pragma solidity >=0.8.0 <0.9.0;
+//SPDX-License-Identifier: MIT
+
+//import "hardhat/console.sol";
+//import "@openzeppelin/contracts/access/Ownable.sol"; //https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
+
+contract YourContract {
+
+  //event SetPurpose(address sender, string purpose);
+
+  string public purpose = "Building Unstoppable Apps";
+
+  constructor() {
+    // what should we do on deploy?
+  }
+
+  function setPurpose(string memory newPurpose) public {
+      purpose = newPurpose;
+      //console.log(msg.sender,"set purpose to",purpose);
+      //emit SetPurpose(msg.sender, purpose);
+  }
+}
+
+{
+  "evmVersion": "istanbul",
+  "libraries": {},
+  "metadata": {
+    "bytecodeHash": "ipfs",
+    "useLiteralContent": true
+  },
+  "optimizer": {
+    "enabled": true,
+    "runs": 200
+  },
+  "remappings": [],
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
