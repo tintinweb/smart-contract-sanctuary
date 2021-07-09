47abcef2168559107576153083d3f56429386975
minion <minion>
2021-07-09 10:12:41 +0000

    with 💕

---
+++ contracts/rinkeby/3f/3fb1c88d71ecac08f46a6ec68cc515b2e00f7f6d_HelloWorld.sol
@@ -0,0 +1,17 @@
+/**
+ *Submitted for verification at Etherscan.io on 2021-07-09
+*/
+
+pragma solidity ^0.4.24;
+
+contract HelloWorld {
+    string public name;
+    
+    constructor() public {
+        name = "Super Ben";
+    }
+    
+    function setName(string _name) public {
+        name = _name;
+    }
+}
