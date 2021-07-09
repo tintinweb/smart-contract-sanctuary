98fb36c1f37f614a920537b1cc6bd1cdad5bb777
auto/updateContractsEtherscanWebKovan <tintinweb@users.noreply.github.com>
2021-07-09 08:13:26 +0000

    auto - updateContractsEtherscanWebKovan (kovan) - autocommit

---
+++ contracts/kovan/a3/a3Fd256eDFB3E92068DC36D2117BE4E8d4779dDa_VerifyFail.sol
@@ -0,0 +1,37 @@
+pragma solidity ^0.8.0;
+
+contract VerifyFail 
+{
+    uint public data;
+    uint256 public createdDate;
+
+    constructor() {
+        data = 0;
+        createdDate = block.timestamp;
+    }
+
+    function set(uint x) public {
+        data = x;
+    }
+
+    function get() public view returns (uint) {
+        return data;    
+    }
+}
+
+{
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "libraries": {}
+}
