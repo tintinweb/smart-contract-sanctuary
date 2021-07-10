5c7c881751019d6fc0a60c0a16b7b387b8f9ed86
auto/updateContractsEtherscanWebRinkeby <tintinweb@users.noreply.github.com>
2021-07-10 14:16:44 +0000

    auto - updateContractsEtherscanWebRinkeby (rinkeby) - autocommit

---
+++ contracts/rinkeby/ce/cEb5BfB5370323b146b236b23887082412971218_GenerateTransfer.sol
@@ -0,0 +1,30 @@
+pragma solidity 0.8.4;
+
+contract GenerateTransfer {
+    event Transfer(address indexed sender, address indexed receiver, uint256 value);
+
+    function gen(uint256 n) external {
+        for (uint256 i = 0; i < n; i++) {
+            emit Transfer(tx.origin, msg.sender, i + 1);
+        }        
+    }
+}
+
+{
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "evmVersion": "istanbul",
+  "remappings": [],
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
