a791c4cd16702735d01fc29e40c40f53622afac0
auto/updateContractsEtherscanWeb <tintinweb@users.noreply.github.com>
2021-07-11 13:19:10 +0000

    auto - updateContractsEtherscanWeb - autocommit

---
+++ contracts/mainnet/6a/6A5c054B9affc9FeEc14887e3cFA2aBe2A58217A_TradeFloor.sol
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2021 The Wolfpack
+ * This file is part of wolves.finance - https://github.com/wolvesofwallstreet/wolves.finance
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * See LICENSE.txt for more information.
+ */
+
+pragma solidity >=0.7.0 <0.8.0;
+
+contract TradeFloor {
+
+  //////////////////////////////////////////////////////////////////////////////
+  // Initialization
+  //////////////////////////////////////////////////////////////////////////////
+
+  constructor(
+    address addressRegistry,
+    address openSeaProxyRegistry
+  ) {
+  }
+
+  /**
+   * @dev One time contract initializer
+   *
+   * @param tokenUriPrefix The ERC-1155 metadata URI Prefix
+   * @param contractUri The contract metadata URI
+   */
+  function initialize(string memory tokenUriPrefix, string memory contractUri)
+    public
+  {
+  }
+}
+
+{
+  "evmVersion": "berlin",
+  "libraries": {},
+  "metadata": {
+    "bytecodeHash": "ipfs",
+    "useLiteralContent": true
+  },
+  "optimizer": {
+    "enabled": true,
+    "runs": 1000
+  },
+  "remappings": [],
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
