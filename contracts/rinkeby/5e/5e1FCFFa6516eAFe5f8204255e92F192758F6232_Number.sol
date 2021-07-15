d3f08c886ec03d50798df6a81f2bc1aba2001f8e
auto/updateContractsEtherscanWebRinkeby <tintinweb@users.noreply.github.com>
2021-07-15 02:20:52 +0000

    auto - updateContractsEtherscanWebRinkeby (rinkeby) - autocommit

---
+++ contracts/rinkeby/5e/5e1FCFFa6516eAFe5f8204255e92F192758F6232_Number.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >= 0.8.0 < 0.9.0;
+
+contract Number {
+
+  uint public mainNumber;
+
+  event NumberChanged(address indexed caller, uint newNumber);
+
+  constructor() {}
+
+  function incrementNumber() external {
+    mainNumber += 1;
+    emit NumberChanged(msg.sender, mainNumber);
+  }
+}
+
+{
+  "metadata": {
+    "bytecodeHash": "none"
+  },
+  "optimizer": {
+    "enabled": true,
+    "runs": 800
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "libraries": {}
+}
