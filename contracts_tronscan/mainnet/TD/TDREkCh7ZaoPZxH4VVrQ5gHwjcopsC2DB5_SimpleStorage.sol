35fa26c2b01bcbaa13dee9b2d2f5a45eac4ec42a
minion <minion>
2021-07-19 23:51:15 +0000

    with 💕

---
+++ contracts_tronscan/mainnet/TD/TDREkCh7ZaoPZxH4VVrQ5gHwjcopsC2DB5_SimpleStorage.sol
@@ -0,0 +1,15 @@
+//SourceUnit: test.sol
+
+pragma solidity ^0.4.18;
+
+contract SimpleStorage {
+    uint storedData;
+
+    function set(uint x) public {
+        storedData = x;
+    }
+
+    function get() public view returns (uint) {
+        return storedData;
+    }
+}
