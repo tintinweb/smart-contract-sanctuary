44e7d0f8fa5417ebd9203af47e5476c230bcb940
minion <minion>
2021-07-19 19:45:22 +0000

    with 💕

---
+++ contracts_bscscan/testnet/1d/1d942cF6694A32E4B3576fC39F501258384F7499_GreetZ.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+
+			pragma solidity 0.6.12;
+
+
+			contract GreetZ {
+			  string greeting;
+
+			  constructor(string memory _greeting) public {
+
+			    greeting = _greeting;
+			  }
+
+			  function greet() public view returns (string memory) {
+			    return greeting;
+			  }
+
+			  function setGreeting(string memory _greeting) public {
+
+			    greeting = _greeting;
+			  }
+			}
+
+{
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "evmVersion": "istanbul",
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
---
+++ contracts_bscscan/testnet/c5/c5ccce07692b5a7c8ef3d4223ec8c3e9e6556b1e_HelloWorld.sol
@@ -0,0 +1,9 @@
+/**
+ *Submitted for verification at BscScan.com on 2021-07-19
+*/
+
+pragma solidity ^0.8.4;
+
+contract HelloWorld {
+    string public greet = "Hello World!";
+}
