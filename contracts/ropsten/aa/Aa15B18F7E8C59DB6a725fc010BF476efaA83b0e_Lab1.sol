7bdb0ec00c4086a590a04d22ea2ca6486c83877a
auto/updateContractsEtherscanWebRopsten <tintinweb@users.noreply.github.com>
2021-07-21 11:10:11 +0000

    auto - updateContractsEtherscanWebRopsten (ropsten) - autocommit

---
+++ contracts/ropsten/aa/Aa15B18F7E8C59DB6a725fc010BF476efaA83b0e_Lab1.sol
@@ -0,0 +1,33 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.6;
+
+contract Lab1 {
+  bool completed = false;
+
+  event DoLab(address indexed from);
+
+  function doLab() public{
+    completed = true;
+    emit DoLab(msg.sender);
+  }
+  function isCompleted() public view returns (bool){
+    return completed;
+  }
+}
+
+{
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "libraries": {}
+}
