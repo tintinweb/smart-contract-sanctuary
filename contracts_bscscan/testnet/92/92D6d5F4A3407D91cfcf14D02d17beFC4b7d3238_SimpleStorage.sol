c7d78e37c63892cb68ffb1dac1f1f7f082574f8a
minion <minion>
2021-07-23 19:47:48 +0000

    with 💕

---
+++ contracts_bscscan/testnet/92/92D6d5F4A3407D91cfcf14D02d17beFC4b7d3238_SimpleStorage.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.8.4;
+
+contract SimpleStorage  {
+    uint data;
+
+    function updateData(uint _data) external {
+        data = _data;
+    }
+
+    function readData() external view returns(uint) {
+        return data;
+    }
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "evmVersion": "istanbul",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
