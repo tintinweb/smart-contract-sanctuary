f1f4672a977b3e6129f828d7cd40a85984dc5388
minion <minion>
2021-07-24 07:34:54 +0000

    with 💕

---
+++ contracts_bscscan/testnet/66/66E33A0F8d49322B2554f651eB8bE811c5B6eAE4_SimpleStorage.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+contract SimpleStorage {
+  bytes private data;
+
+  function updateData(bytes memory _data) external {
+    data = _data;
+  }
+  function readData() external view returns(bytes memory) {
+    return data;
+  }
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "evmVersion": "istanbul",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
