cbbcacf7f7b026efb31007ca9372b50b4c906d1c
minion <minion>
2021-08-09 16:37:19 +0000

    with 💕

---
+++ contracts_bscscan/testnet/1a/1AddC6370f996CD22d0353Bb98f6a5F838B25d7A_ClaimOwnership.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract ClaimOwnership {
+    address public owner;
+
+    bool private isInitialized;
+
+    constructor() {}
+
+    function init() external {
+        require(!isInitialized, "ClaimOwnership: Initialized");
+
+        owner = msg.sender;
+    }
+
+    function changeOwnership(address newOwner) external {
+        address _ownerOld = owner;
+
+        require(msg.sender == _ownerOld, "ClaimOwnership: Not allowed");
+        require(newOwner != _ownerOld, "ClaimOwnership: Diff");
+
+        owner = newOwner;
+    }
+}
+
+{
+  "optimizer": {
+    "enabled": true,
+    "runs": 999999
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "libraries": {}
+}
