821b616220633a57bc67ae8a659720cdca915c71
minion <minion>
2021-08-10 06:48:11 +0000

    with 💕

---
+++ contracts_bscscan/testnet/b2/B293174E300209Ab1F57E0eECD6FDCe22D6F50F3_RandomGenUtils.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.7.0;
+
+contract RandomGenUtils {
+    function randomGen(uint256 seed, uint256 max) internal view returns (uint256 randomNumber) {
+        return (uint256(
+            keccak256(
+                abi.encodePacked(blockhash(block.number - 1), block.timestamp, msg.sender, block.difficulty, seed)
+            )
+        ) % max);
+    }
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": true,
+    "runs": 200
+  },
+  "evmVersion": "istanbul",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
