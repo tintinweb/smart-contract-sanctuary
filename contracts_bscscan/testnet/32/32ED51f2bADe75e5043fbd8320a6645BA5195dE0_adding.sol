6f22893c531526d0fb49b6793c5458fc792b1073
minion <minion>
2021-08-12 06:49:57 +0000

    with 💕

---
+++ contracts_bscscan/testnet/32/32ED51f2bADe75e5043fbd8320a6645BA5195dE0_adding.sol
@@ -0,0 +1,39 @@
+pragma solidity 0.6.6;
+
+contract adding{
+    
+    event plus(
+        uint256 number
+    );
+    uint256 public b;
+    constructor () public{
+        b = 0;
+    }
+    
+    function add ()
+        public
+    {
+        b += 1;
+        emit plus(b);
+    }
+}
+
+{
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "metadata": {
+    "useLiteralContent": true
+  },
+  "libraries": {}
+}
---
+++ contracts_bscscan/testnet/b6/B6215288097C36395c8d52f502819D91222Bb2F0_MyTest.sol
@@ -0,0 +1,36 @@
+pragma solidity 0.8.0;
+
+contract MyTest {
+  uint value;
+
+  function setValue(uint _value) public {
+    value = _value;
+  }
+
+  function getValue() public view returns (uint) {
+    return value;
+  }
+
+  function getValueX2() public view returns (uint) {
+    return value * 2;
+  }
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": true,
+    "runs": 200
+  },
+  "evmVersion": "istanbul",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
