017ffa56d56072d66b5d7b49fc9901080a97a9c7
minion <minion>
2021-08-13 06:42:22 +0000

    with 💕

---
+++ contracts_bscscan/testnet/93/93E35afE9C21b946666404eEf2497A297966538e_SimpleTest.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+contract SimpleTest {
+    uint public x;
+    constructor(uint a) payable {
+        x = a;
+    }
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": true,
+    "runs": 999
+  },
+  "evmVersion": "istanbul",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
