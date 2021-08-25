50f4920708423a21c84430d97897eced247c4477
minion <minion>
2021-08-25 06:06:57 +0000

    with 💕

---
+++ contracts_polygonscan/mumbai/42/429F543332d3b5ad2e30156De58Dc180E3184356_ConvertLib.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.25 <0.9.0;
+
+library ConvertLib {
+	struct TestStruct {
+		uint256 name;
+	}
+
+	function convert(uint amount,uint conversionRate) public pure returns (uint convertedAmount)
+	{
+		return amount * conversionRate;
+	}
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": true,
+    "runs": 200
+  },
+  "evmVersion": "byzantium",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
