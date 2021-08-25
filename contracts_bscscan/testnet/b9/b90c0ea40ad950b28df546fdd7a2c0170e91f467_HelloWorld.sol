577a68006518d0834565d93ce82fce57029d7e2e
minion <minion>
2021-08-25 06:51:16 +0000

    with 💕

---
+++ contracts_bscscan/testnet/b9/b90c0ea40ad950b28df546fdd7a2c0170e91f467_HelloWorld.sol
@@ -0,0 +1,17 @@
+/**
+ *Submitted for verification at BscScan.com on 2021-08-24
+*/
+
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+contract HelloWorld {
+  string public message;
+
+  constructor(string memory initialiseMessage) {
+    message = initialiseMessage;
+  }
+
+  function updateStr(string memory newMessage) public {
+    message = newMessage;
+  }
+}
