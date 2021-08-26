5fa69f4a491ebe4f0865a4f5f28ac924e2e5dee1
minion <minion>
2021-08-26 06:50:33 +0000

    with 💕

---
+++ contracts_bscscan/testnet/ae/aeA6D5112b7e171e70469B777AD76bF4E8d4497e_Migrations.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.5;
+
+contract Migrations {
+    address public owner = msg.sender;
+    uint256 public last_completed_migration;
+
+    modifier restricted() {
+        require(
+            msg.sender == owner,
+            "This function is restricted to the contract's owner"
+        );
+        _;
+    }
+
+    function setCompleted(uint256 completed) public restricted {
+        last_completed_migration = completed;
+    }
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": true,
+    "runs": 200
+  },
+  "evmVersion": "petersburg",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
