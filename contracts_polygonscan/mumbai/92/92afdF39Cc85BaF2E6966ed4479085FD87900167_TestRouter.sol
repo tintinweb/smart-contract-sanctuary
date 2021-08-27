9f200d5a607054cdbc04b47661663de806925dc9
minion <minion>
2021-08-27 14:15:28 +0000

    with 💕

---
+++ contracts_polygonscan/mumbai/92/92afdF39Cc85BaF2E6966ed4479085FD87900167_TestRouter.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.8.6;
+
+
+contract TestRouter {
+    address public immutable WETH = 0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619;
+
+    constructor(){}
+
+    function getAmountsOut(uint amountIn, address[] calldata path) external view returns (uint[] memory amounts){
+        amounts = new uint[](path.length);
+
+        amounts[path.length-1] = amountIn;
+    }
+
+}
+
+{
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "libraries": {}
+}
