37deb8bfc2a64c3e00741b45cb483c513b52ffea
minion <minion>
2021-08-27 14:40:14 +0000

    with 💕

---
+++ contracts_bscscan/testnet/59/59e944fbc323980e8cdcdf9c9dcd075a9823ce62_Locker.sol
@@ -0,0 +1,11 @@
+/**
+ *Submitted for verification at BscScan.com on 2021-08-27
+*/
+
+pragma solidity ^0.8.0;
+
+contract Locker {
+    function newLocker(address token, uint time, uint amount, string calldata image) public payable {
+        
+    }
+}
