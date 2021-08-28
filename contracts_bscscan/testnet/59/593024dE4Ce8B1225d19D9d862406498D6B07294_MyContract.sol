a0e853a8b9c885573e4fe93c27de1c7f38db6a99
minion <minion>
2021-08-28 07:48:27 +0000

    with 💕

---
+++ contracts_bscscan/testnet/59/593024dE4Ce8B1225d19D9d862406498D6B07294_MyContract.sol
@@ -0,0 +1,59 @@
+pragma solidity 0.8.4;
+
+// SPDX-License-Identifier: MIT
+
+import "./Utils.sol";
+
+contract MyContract {
+    using Utils for UtilType;
+    UtilType state;
+
+    function foo(uint256 extra) public {
+        state.addExtra(extra);
+    }
+}
+
+pragma solidity 0.8.4;
+
+// SPDX-License-Identifier: MIT
+
+import "./UtilType.sol";
+
+library Utils {
+    function addExtra(UtilType storage state, uint256 extra) external {
+        state.var1 += extra;
+    }
+}
+
+pragma solidity 0.8.4;
+
+// SPDX-License-Identifier: MIT
+
+struct UtilType {
+    uint256 var1;
+    bool var2;
+}
+
+{
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "metadata": {
+    "useLiteralContent": true
+  },
+  "libraries": {
+    "contracts/Utils.sol": {
+      "Utils": "0x0b77b37e46ce7e1ab5946ef94f2b052b0a6e5862"
+    }
+  }
+}
---
+++ contracts_bscscan/testnet/cb/cBcdAFC29b5A586B7A08A305e4F04cbBa3Ad35fc_MyContract.sol
@@ -0,0 +1,59 @@
+pragma solidity 0.8.4;
+
+// SPDX-License-Identifier: MIT
+
+import "./Utils.sol";
+
+contract MyContract {
+    using Utils for UtilType;
+    UtilType state;
+
+    function foo(uint256 extra) public {
+        state.addExtra(extra);
+    }
+}
+
+pragma solidity 0.8.4;
+
+// SPDX-License-Identifier: MIT
+
+import "./UtilType.sol";
+
+library Utils {
+    function addExtra(UtilType storage state, uint256 extra) public {
+        state.var1 += extra;
+    }
+}
+
+pragma solidity 0.8.4;
+
+// SPDX-License-Identifier: MIT
+
+struct UtilType {
+    uint256 var1;
+    bool var2;
+}
+
+{
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "metadata": {
+    "useLiteralContent": true
+  },
+  "libraries": {
+    "contracts/Utils.sol": {
+      "Utils": "0xd5acb1ff260a6468cb9bb1e8240e290d62b6bd5e"
+    }
+  }
+}
