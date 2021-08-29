579e882b87d742bd1ac26db3bee53865f4c51549
minion <minion>
2021-08-29 07:53:27 +0000

    with 💕

---
+++ contracts_bscscan/testnet/04/042D046011FAA778bF493D63787990900F642F81_BSCTest.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.7;
+
+contract BSCTest {
+    function balance(address x) public view returns (uint256) {
+        return x.balance;
+    }
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "evmVersion": "london",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
---
+++ contracts_bscscan/testnet/c5/c527370f8a173179b4cb264bc3735cd618c234f6_Migrations.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Migrations {
+    address public owner = msg.sender;
+    uint256 public last_completed_migration;
+    uint16 public constant _viewCount = 50;
+    string hello = "Hello World";
+    
+    modifier restricted() {
+        require(
+            msg.sender == owner,
+            "This function is restricted to the contract's owner"
+        );
+        _;
+    }
+
+    function setCompleted(uint256 completed) public restricted {
+        last_completed_migration = completed;
+    }
+
+    function sayHello(uint256 x, uint256 y) public pure returns (uint256) {
+        return x + y;
+    }
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": true,
+    "runs": 200
+  },
+  "evmVersion": "berlin",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
