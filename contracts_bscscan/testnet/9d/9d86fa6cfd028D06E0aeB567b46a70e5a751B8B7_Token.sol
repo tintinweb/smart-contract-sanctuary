f50745efebdd1e865fdb288cf7c52d7a5e396880
minion <minion>
2021-08-30 07:34:43 +0000

    with 💕

---
+++ contracts_bscscan/testnet/9d/9d86fa6cfd028D06E0aeB567b46a70e5a751B8B7_Token.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+contract Token {
+    string public name;
+
+    constructor (string memory _name) {
+        name = _name;
+    }
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": true,
+    "runs": 200
+  },
+  "evmVersion": "london",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
