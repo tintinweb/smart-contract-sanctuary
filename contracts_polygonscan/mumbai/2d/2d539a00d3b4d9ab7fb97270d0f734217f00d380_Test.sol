9dcd65092a9c7c77378e0944179cdb4820abafa0
minion <minion>
2021-08-31 07:09:46 +0000

    with 💕

---
+++ contracts_polygonscan/mumbai/2d/2d539a00d3b4d9ab7fb97270d0f734217f00d380_Test.sol
@@ -0,0 +1,9 @@
+/**
+ *Submitted for verification at polygonscan.com on 2021-08-31
+*/
+
+contract Test{
+    function WhatIsMyAddress() public view returns (address){
+        return msg.sender;
+    }
+}
