b0e1610a06ca5c51bd971da49f094bd86e1ce675
minion <minion>
2021-09-04 06:52:45 +0000

    with 💕

---
+++ contracts_bscscan/testnet/9d/9dCBf1Ba54a64a31eD9D0e59677DAb0072a1D5b6_UyangData.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract UyangData{
+
+	address owner;
+	mapping (address=>uint) public uyangOfAmout;
+	uint public totalUyangAmout;
+	uint public totalAddress;
+
+	mapping (address => bool) public approveContracts;
+
+	modifier onlyOwner() {
+		require(msg.sender == owner);
+		_;
+    }
+
+	constructor() {
+		owner = msg.sender;
+	}
+
+	function toApproveContract(address platform, bool isApprove) public onlyOwner{
+		approveContracts[platform] = isApprove;
+	}
+
+	function increaseUyang(address player, uint amout) public {
+		require(approveContracts[msg.sender], "the contract do not approve");
+		if (uyangOfAmout[player] == 0) {
+			totalAddress ++;
+		}
+		uyangOfAmout[player] += amout;
+	}
+
+	function decreaseUyang(address player, uint amout) public {
+		require(approveContracts[msg.sender], "the contract do not approve");
+		uyangOfAmout[player] -= amout;
+	}
+
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": true,
+    "runs": 1
+  },
+  "evmVersion": "byzantium",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
---
+++ contracts_bscscan/testnet/9e/9E3084F50df4c07F85053fAA03746F5476602Ea7_GedditSeedSale.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+interface IWhitelist {
+  function isWhitelisted(address investor) external view returns (bool);
+}
+
+contract GedditSeedSale {
+  // The token being sold
+  IWhitelist  _whitelist;
+
+  constructor(IWhitelist whitelist){
+    _whitelist = whitelist;
+  }
+
+  function iswhite(address a) public view returns (bool) {
+    return IWhitelist(_whitelist).isWhitelisted(a);
+  }
+
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "evmVersion": "london",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
