47beb8c9e64801cd37922d96ae9f8971fc57c5d2
minion <minion>
2021-09-07 06:47:35 +0000

    with 💕

---
+++ contracts_bscscan/testnet/58/58Bc2812f0FE397E000Ae1BfB04BaA9887355Bea_Test.sol
@@ -0,0 +1,29 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.6;
+
+contract Test{
+    constructor(){
+    }
+
+    function getMonthSinceLastClaim(uint256 lastClaim) internal view returns(uint256){
+        return (block.timestamp - lastClaim) / (30 days);
+    }
+
+}
+
+{
+  "optimizer": {
+    "enabled": true,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "libraries": {}
+}
