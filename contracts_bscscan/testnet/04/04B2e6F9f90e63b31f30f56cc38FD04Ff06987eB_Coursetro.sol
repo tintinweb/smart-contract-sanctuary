f828ebd3131b4dccb15696f4988a5de272e1a5ba
minion <minion>
2021-09-09 06:52:06 +0000

    with 💕

---
+++ contracts_bscscan/testnet/04/04B2e6F9f90e63b31f30f56cc38FD04Ff06987eB_Coursetro.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Coursetro {
+
+    string fName;
+    uint age;
+
+    event Instructor(
+        string  name,
+        uint age
+    );
+
+    function setInstructor(string memory _fName, uint _age) public {
+        fName = _fName;
+        age = _age;
+        emit Instructor(_fName, _age);
+    }
+
+    function getInstructor() view public returns (string memory, uint) {
+        return (fName, age);
+    }
+
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "evmVersion": "istanbul",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
---
+++ contracts_bscscan/testnet/8d/8D78475b130662852842b17d7d557E1F2146b13A_Test.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Test{
+
+    mapping(address => uint) balances;
+
+    function deposit() public payable{
+        balances[msg.sender] = msg.value;
+    }
+
+    function withdraw(uint value) public{
+        require(balances[msg.sender] >= value, "The Balances is not enough");
+        (msg.sender).transfer(value);
+        balances[msg.sender] -= value;
+    }
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": true,
+    "runs": 200
+  },
+  "evmVersion": "istanbul",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
