4ab419873a28e9990e4768407032d1b026290af4
minion <minion>
2021-09-10 06:09:10 +0000

    with 💕

---
+++ contracts_polygonscan/mumbai/81/81C06B65f320cF104C44590E667424A11D6196B3_ABC.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.8.0;
+
+// SPDX-License-Identifier: MIT
+
+
+
+contract ABC {
+
+    uint256 public c;
+    constructor(uint256 cap) {
+        c = cap;
+    }
+}
+
+{
+  "optimizer": {
+    "enabled": true,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "libraries": {}
+}
