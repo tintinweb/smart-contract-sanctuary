611b12912726dab5b662260d7d71db613dacfae3
minion <minion>
2021-09-11 06:51:15 +0000

    with 💕

---
+++ contracts_bscscan/testnet/a5/a5c2b5a8156a5d922441e01c0946dc20b2289e09_ZombieFactory.sol
@@ -0,0 +1,49 @@
+pragma solidity ^0.4.19;
+
+import "./ownable.sol";
+import "./safemath.sol";
+
+contract ZombieFactory is Ownable {
+    using SafeMath for uint256;
+
+    event NewZombie(uint256 zombieId, string name, uint256 dna);
+
+    uint256 dnaDigits = 16;
+    uint256 dnaModulus = 10**dnaDigits;
+    uint256 cooldownTime = 1 days;
+
+    struct Zombie {
+        string name;
+        uint256 dna;
+        uint32 level;
+        uint32 readyTime;
+        uint16 winCount;
+        uint16 lossCount;
+    }
+
+    Zombie[] public zombies;
+
+    mapping(uint256 => address) public zombieToOwner;
+    mapping(address => uint256) ownerZombieCount;
+
+    function _createZombie(string _name, uint256 _dna) internal {
+        uint256 id = zombies.push(
+            Zombie(_name, _dna, 1, uint32(now + cooldownTime), 0, 0)
+        ) - 1;
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
+        NewZombie(id, _name, _dna);
+    }
+
+    function _generateRandomDna(string _str) private view returns (uint256) {
+        uint256 rand = uint256(keccak256(_str));
+        return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string _name) public {
+        require(ownerZombieCount[msg.sender] == 0);
+        uint256 randDna = _generateRandomDna(_name);
+        randDna = randDna - (randDna % 100);
+        _createZombie(_name, randDna);
+    }
+}
---
+++ contracts_bscscan/testnet/d6/d6F354935f1C62384a301989Ae25A28Cfa327BfC_Time.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.8.0;
+
+contract Time {
+    function GetTimeNow() public view returns (uint256) {
+        return block.timestamp;
+    }
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "evmVersion": "istanbul",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
