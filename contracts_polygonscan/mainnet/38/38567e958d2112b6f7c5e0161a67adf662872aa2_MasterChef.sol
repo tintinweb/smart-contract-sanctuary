bdee406d6b99cecb55038cca13e3c7a2be662412
minion <minion>
2021-09-12 20:17:09 +0000

    with 💕

---
+++ contracts_polygonscan/mainnet/38/38567e958d2112b6f7c5e0161a67adf662872aa2_MasterChef.sol
@@ -0,0 +1,248 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.12;
+
+import "./SafeMath.sol";
+import "./IBEP20.sol";
+import "./SafeBEP20.sol";
+import "./Ownable.sol";
+
+import "./KleoToken.sol";
+
+// Goose fork.
+// Some infos for scanners:
+// No deflationary/transfer-tax token pool will be added we are aware of the issue
+// Deflationary mecanism for Kleo Token : Buybacks and marketing.
+// Deposit fee capped to 2.5%
+// No reward minted/No token sent to dev adress but the fees.
+// No Anti-whale / No referal /  No harvest lock (for this layer)
+// God Bless.
+
+
+contract MasterChef is Ownable {
+    using SafeMath for uint256;
+    using SafeBEP20 for IBEP20;
+
+    // Info of each user.
+    struct UserInfo {
+        uint256 amount;         // How many LP tokens the user has provided.
+        uint256 rewardDebt;     // Reward debt. See explanation below.
+        //
+        // We do some fancy math here. Basically, any point in time, the amount of KLEOs
+        // entitled to a user but is pending to be distributed is:
+        //
+        //   pending reward = (user.amount * pool.accKleoPerShare) - user.rewardDebt
+        //
+        // Whenever a user deposits or withdraws LP tokens to a pool. Here's what happens:
+        //   1. The pool's `accKleoPerShare` (and `lastRewardBlock`) gets updated.
+        //   2. User receives the pending reward sent to his/her address.
+        //   3. User's `amount` gets updated.
+        //   4. User's `rewardDebt` gets updated.
+    }
+
+    // Info of each pool.
+    struct PoolInfo {
+        IBEP20 lpToken;           // Address of LP token contract.
+        uint256 allocPoint;       // How many allocation points assigned to this pool. KLEO to distribute per block.
+        uint256 lastRewardBlock;  // Last block number that KLEO distribution occurs.
+        uint256 accKleoPerShare;   // Accumulated Kleos per share, times 1e12. See below.
+        uint16 depositFeeBP;      // Deposit fee in basis points
+    }
+
+    // The KLEO TOKEN!
+    KleoToken public kleo;
+    // Dev address.
+    address public devaddr;
+    // KLEO tokens created per block.
+    uint256 public kleoPerBlock;
+    // Bonus muliplier for early KLEO makers.
+    uint256 public constant BONUS_MULTIPLIER = 1;
+    // Deposit Fee address
+    address public feeAddress;
+
+    // Info of each pool.
+    PoolInfo[] public poolInfo;
+    // Info of each user that stakes LP tokens.
+    mapping (uint256 => mapping (address => UserInfo)) public userInfo;
+    // Total allocation points. Must be the sum of all allocation points in all pools.
+    uint256 public totalAllocPoint = 0;
+    // The block number when KLEO mining starts.
+    uint256 public startBlock;
+
+    event Deposit(address indexed user, uint256 indexed pid, uint256 amount);
+    event Withdraw(address indexed user, uint256 indexed pid, uint256 amount);
+    event EmergencyWithdraw(address indexed user, uint256 indexed pid, uint256 amount);
+
+    constructor(
+        KleoToken _kleo,
+        address _devaddr,
+        address _feeAddress,
+        uint256 _kleoPerBlock,
+        uint256 _startBlock
+    ) public {
+        kleo = _kleo;
+        devaddr = _devaddr;
+        feeAddress = _feeAddress;
+        kleoPerBlock = _kleoPerBlock;
+        startBlock = _startBlock;
+    }
+
+    function poolLength() external view returns (uint256) {
+        return poolInfo.length;
+    }
+
+    // Add a new lp to the pool. Can only be called by the owner.
+    // DO NOT add the same LP token more than once. Rewards will be messed up if you do.
+    function add(uint256 _allocPoint, IBEP20 _lpToken, uint16 _depositFeeBP, bool _withUpdate) public onlyOwner {
+        require(_depositFeeBP <= 250, "add: invalid deposit fee basis points");
+        if (_withUpdate) {
+            massUpdatePools();
+        }
+        uint256 lastRewardBlock = block.number > startBlock ? block.number : startBlock;
+        totalAllocPoint = totalAllocPoint.add(_allocPoint);
+        poolInfo.push(PoolInfo({
+            lpToken: _lpToken,
+            allocPoint: _allocPoint,
+            lastRewardBlock: lastRewardBlock,
+            accKleoPerShare: 0,
+            depositFeeBP: _depositFeeBP
+        }));
+    }
+
+    // Update the given pool's KLEO allocation point and deposit fee. Can only be called by the owner.
+    function set(uint256 _pid, uint256 _allocPoint, uint16 _depositFeeBP, bool _withUpdate) public onlyOwner {
+        require(_depositFeeBP <= 250, "set: invalid deposit fee basis points");
+        if (_withUpdate) {
+            massUpdatePools();
+        }
+        totalAllocPoint = totalAllocPoint.sub(poolInfo[_pid].allocPoint).add(_allocPoint);
+        poolInfo[_pid].allocPoint = _allocPoint;
+        poolInfo[_pid].depositFeeBP = _depositFeeBP;
+    }
+
+    // Return reward multiplier over the given _from to _to block.
+    function getMultiplier(uint256 _from, uint256 _to) public view returns (uint256) {
+        return _to.sub(_from).mul(BONUS_MULTIPLIER);
+    }
+
+    // View function to see pending KLEO on frontend.
+    function pendingKleo(uint256 _pid, address _user) external view returns (uint256) {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][_user];
+        uint256 accKleoPerShare = pool.accKleoPerShare;
+        uint256 lpSupply = pool.lpToken.balanceOf(address(this));
+        if (block.number > pool.lastRewardBlock && lpSupply != 0) {
+            uint256 multiplier = getMultiplier(pool.lastRewardBlock, block.number);
+            uint256 kleoReward = multiplier.mul(kleoPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
+            accKleoPerShare = accKleoPerShare.add(kleoReward.mul(1e12).div(lpSupply));
+        }
+        return user.amount.mul(accKleoPerShare).div(1e12).sub(user.rewardDebt);
+    }
+
+    // Update reward variables for all pools. Be careful of gas spending!
+    function massUpdatePools() public {
+        uint256 length = poolInfo.length;
+        for (uint256 pid = 0; pid < length; ++pid) {
+            updatePool(pid);
+        }
+    }
+
+    // Update reward variables of the given pool to be up-to-date.
+    function updatePool(uint256 _pid) public {
+        PoolInfo storage pool = poolInfo[_pid];
+        if (block.number <= pool.lastRewardBlock) {
+            return;
+        }
+        uint256 lpSupply = pool.lpToken.balanceOf(address(this));
+        if (lpSupply == 0 || pool.allocPoint == 0) {
+            pool.lastRewardBlock = block.number;
+            return;
+        }
+        uint256 multiplier = getMultiplier(pool.lastRewardBlock, block.number);
+        uint256 kleoReward = multiplier.mul(kleoPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
+        kleo.mint(address(this), kleoReward);
+        pool.accKleoPerShare = pool.accKleoPerShare.add(kleoReward.mul(1e12).div(lpSupply));
+        pool.lastRewardBlock = block.number;
+    }
+
+    // Deposit LP tokens to MasterChef for KLEO allocation.
+    function deposit(uint256 _pid, uint256 _amount) public {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        updatePool(_pid);
+        if (user.amount > 0) {
+            uint256 pending = user.amount.mul(pool.accKleoPerShare).div(1e12).sub(user.rewardDebt);
+            if(pending > 0) {
+                safeKleoTransfer(msg.sender, pending);
+            }
+        }
+        if(_amount > 0) {
+            pool.lpToken.safeTransferFrom(address(msg.sender), address(this), _amount);
+            if(pool.depositFeeBP > 0){
+                uint256 depositFee = _amount.mul(pool.depositFeeBP).div(10000);
+                pool.lpToken.safeTransfer(feeAddress, depositFee);
+                user.amount = user.amount.add(_amount).sub(depositFee);
+            }else{
+                user.amount = user.amount.add(_amount);
+            }
+        }
+        user.rewardDebt = user.amount.mul(pool.accKleoPerShare).div(1e12);
+        emit Deposit(msg.sender, _pid, _amount);
+    }
+
+    // Withdraw LP tokens from MasterChef.
+    function withdraw(uint256 _pid, uint256 _amount) public {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        require(user.amount >= _amount, "withdraw: not good");
+        updatePool(_pid);
+        uint256 pending = user.amount.mul(pool.accKleoPerShare).div(1e12).sub(user.rewardDebt);
+        if(pending > 0) {
+            safeKleoTransfer(msg.sender, pending);
+        }
+        if(_amount > 0) {
+            user.amount = user.amount.sub(_amount);
+            pool.lpToken.safeTransfer(address(msg.sender), _amount);
+        }
+        user.rewardDebt = user.amount.mul(pool.accKleoPerShare).div(1e12);
+        emit Withdraw(msg.sender, _pid, _amount);
+    }
+
+    // Withdraw without caring about rewards. EMERGENCY ONLY.
+    function emergencyWithdraw(uint256 _pid) public {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        uint256 amount = user.amount;
+        user.amount = 0;
+        user.rewardDebt = 0;
+        pool.lpToken.safeTransfer(address(msg.sender), amount);
+        emit EmergencyWithdraw(msg.sender, _pid, amount);
+    }
+
+    // Safe KLEO transfer function, just in case if rounding error causes pool to not have enough KLEO.
+    function safeKleoTransfer(address _to, uint256 _amount) internal {
+        uint256 kleoBal = kleo.balanceOf(address(this));
+        if (_amount > kleoBal) {
+            kleo.transfer(_to, kleoBal);
+        } else {
+            kleo.transfer(_to, _amount);
+        }
+    }
+
+    // Update dev address by the previous dev.
+    function dev(address _devaddr) public {
+        require(msg.sender == devaddr, "dev: wut?");
+        devaddr = _devaddr;
+    }
+
+    function setFeeAddress(address _feeAddress) public{
+        require(msg.sender == feeAddress, "setFeeAddress: FORBIDDEN");
+        feeAddress = _feeAddress;
+    }
+
+    //Pancake has to add hidden dummy pools inorder to alter the emission, here we make it simple and transparent to all.
+    function updateEmissionRate(uint256 _kleoPerBlock) public onlyOwner {
+        massUpdatePools();
+        kleoPerBlock = _kleoPerBlock;
+    }
+}
---
+++ contracts_polygonscan/mainnet/84/844B11C1a77A2E366b4C511FE82fD9Cef5da6A1D_PoolDowserV1.sol
@@ -0,0 +1,148 @@
+// SPDX-License-Identifier: MIT
+
+/*
+Join us at PolyCrystal.Finance!
+█▀▀█ █▀▀█ █░░ █░░█ █▀▀ █▀▀█ █░░█ █▀▀ ▀▀█▀▀ █▀▀█ █░░ 
+█░░█ █░░█ █░░ █▄▄█ █░░ █▄▄▀ █▄▄█ ▀▀█ ░░█░░ █▄▄█ █░░ 
+█▀▀▀ ▀▀▀▀ ▀▀▀ ▄▄▄█ ▀▀▀ ▀░▀▀ ▄▄▄█ ▀▀▀ ░░▀░░ ▀░░▀ ▀▀▀
+*/
+
+import "./IPoolV1.sol";
+import "./DowsingTokenV1.sol";
+import "./Ownable.sol";
+import "./IERC20Metadata.sol";
+pragma solidity ^0.8.6;
+
+contract PoolDowserV1 is Ownable {
+    
+    bytes32 constant public TOKEN_CODE_HASH = keccak256(type(DowsingTokenV1).creationCode);
+    IPoolV1[] public pools;
+    
+    struct SymbolOverride {
+        bytes short;
+        bytes long;
+    }
+
+    mapping (IERC20 => SymbolOverride) public overrides;
+    
+    //replaces instances of the token symbol with these when forming tracker token names/symbols.
+    //Empty strings here clear a previous override and reset to the normal symbol
+    function _override(IERC20 token, string memory _short, string memory _long) external onlyOwner {
+        SymbolOverride storage tokenOverride = overrides[token];
+        if (bytes(_short).length > 0) tokenOverride.short = bytes(_short);
+        if (bytes(_long).length > 0) tokenOverride.long = bytes(_long);
+    }
+    
+    function statusPrefix(IPoolV1 pool) public view returns (string memory prefix) {
+        uint startBlock = pool.startBlock();
+        if (block.number < startBlock) return unicode"✨";
+        uint endBlock = pool.bonusEndBlock();
+        if (block.number >= pool.bonusEndBlock()) return unicode"⛔"; 
+        if (pool.rewardBalance() < uint(endBlock - block.number) * pool.rewardPerBlock()) return unicode"⚠️";
+        else return "";
+    }
+
+    function shortSymbol(IERC20Metadata token) internal view returns (bytes memory symbol) {
+        if (overrides[token].short.length == 0) {
+            try token.symbol() returns (string memory _symbol) {
+                return bytes(_symbol);
+            } catch {
+                return "";
+            }
+        } else {
+            return overrides[token].short;
+        }
+    }
+    function longSymbol(IERC20Metadata token) internal view returns (bytes memory symbol) {
+        if (overrides[token].long.length == 0) {
+            try token.symbol() returns (string memory _symbol) {
+                return bytes(_symbol);
+            } catch {
+                return "";
+            }
+        } else {
+            return overrides[token].long;
+        }
+    }
+    
+    //forms a string to represent user staked tokens for a pool
+    function getNameStaked(IPoolV1 pool) public view returns (string memory name) {
+        IERC20Metadata stakeToken = IERC20Metadata(address(pool.STAKE_TOKEN()));
+        IERC20Metadata earnedToken = IERC20Metadata(address(pool.REWARD_TOKEN()));
+        bytes memory stakeSymbol = longSymbol(stakeToken);
+        bytes memory earnedSymbol = longSymbol(earnedToken);
+
+        return string(abi.encodePacked(statusPrefix(pool), stakeSymbol, " staked to earn ", earnedSymbol));
+    }
+    //forms a string representing pending rewards for a pool
+    function getNamePending(IPoolV1 pool) public view returns (string memory name) {
+        IERC20Metadata stakeToken = IERC20Metadata(address(pool.STAKE_TOKEN()));
+        IERC20Metadata earnedToken = IERC20Metadata(address(pool.REWARD_TOKEN()));
+        bytes memory stakeSymbol = longSymbol(stakeToken);
+        bytes memory earnedSymbol = longSymbol(earnedToken);
+
+        return string(abi.encodePacked(statusPrefix(pool), earnedSymbol, " pending from staking ", stakeSymbol));
+    }
+    //forms a shorter string to represent user staked tokens for a pool
+    function getSymbolStaked(IPoolV1 pool) public view returns (string memory name) {
+        IERC20Metadata stakeToken = IERC20Metadata(address(pool.STAKE_TOKEN()));
+        IERC20Metadata earnedToken = IERC20Metadata(address(pool.REWARD_TOKEN()));
+        bytes memory stakeSymbol = shortSymbol(stakeToken);
+        bytes memory earnedSymbol = shortSymbol(earnedToken);
+
+        return string(abi.encodePacked(statusPrefix(pool), stakeSymbol, unicode"→", earnedSymbol));        
+    }
+    //forms a shorter string representing pending rewards for a pool
+    function getSymbolPending(IPoolV1 pool) public view returns (string memory name) {
+        IERC20Metadata stakeToken = IERC20Metadata(address(pool.STAKE_TOKEN()));
+        IERC20Metadata earnedToken = IERC20Metadata(address(pool.REWARD_TOKEN()));
+        bytes memory stakeSymbol = shortSymbol(stakeToken);
+        bytes memory earnedSymbol = shortSymbol(earnedToken);
+
+        return string(abi.encodePacked(statusPrefix(pool), unicode"⌛", earnedSymbol, " (", stakeSymbol, ")"));
+    }
+    
+    function sweepToken(IERC20 token) external onlyOwner {
+        uint256 balance = token.balanceOf(address(this));
+        token.transfer(msg.sender, balance);
+    }
+    
+    function addPool(IPoolV1 pool) external onlyOwner {
+        bytes memory code = type(DowsingTokenV1).creationCode;
+        bytes32 salt = keccak256(abi.encodePacked(false, pool));
+        DowsingTokenV1 token;
+        assembly {
+            token := create2(0, add(code, 0x20), mload(code), salt)
+            if iszero(extcodesize(token)) { revert(0, 0) }
+        }
+        token.initialize(pool, false);
+        salt = keccak256(abi.encodePacked(true, pool));
+        assembly {
+            token := create2(0, add(code, 0x20), mload(code), salt)
+            if iszero(extcodesize(token)) { revert(0, 0) }
+        }
+        token.initialize(pool, true);
+        pools.push() = pool;
+    }
+    function findTokens(IPoolV1 pool) public view returns (DowsingTokenV1 stakedToken, DowsingTokenV1 pendingToken) {
+        return (
+            DowsingTokenV1(address(uint160(uint(keccak256(abi.encodePacked(hex'ff',address(this),keccak256(abi.encodePacked(false,pool)),TOKEN_CODE_HASH)))))),
+            DowsingTokenV1(address(uint160(uint(keccak256(abi.encodePacked(hex'ff',address(this),keccak256(abi.encodePacked(true,pool)),TOKEN_CODE_HASH))))))
+        );
+    }
+    function _destroy(IPoolV1 pool) internal {
+        (DowsingTokenV1 staked, DowsingTokenV1 pending) = findTokens(pool);
+        try staked.destroy() {} catch {}
+        try pending.destroy() {} catch {}
+    }
+    function destroy(IPoolV1 pool) external onlyOwner {
+        _destroy(pool);
+    }
+    function destroyAll() external onlyOwner {
+        for (uint i; i < pools.length; i++) {
+            _destroy(pools[i]);
+        }
+        selfdestruct(payable(owner()));
+    }
+
+}
