3cd6623a781bbedc05ef0f2ab04ea501cc683ef2
minion <minion>
2021-09-13 06:25:05 +0000

    with 💕

---
+++ contracts_bscscan/testnet/32/3244170276008B047269cfab20a32941a7524EBA_MaGo.sol
@@ -0,0 +1,27 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.7;
+
+contract MaGo{
+    uint256 number;
+
+    function getNum() public view returns (uint256){
+        return number;
+    }
+}
+
+{
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "libraries": {}
+}
