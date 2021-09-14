7bd67e9f28efd4542eb97fd619495bb6a812f832
minion <minion>
2021-09-14 07:37:18 +0000

    with 💕

---
+++ contracts_bscscan/testnet/2a/2AAB14ba074EaC1c923e2814d8B79F39C3875d8f_Forwarder.sol
@@ -0,0 +1,26 @@
+pragma solidity 0.8.2;
+
+contract Forwarder{
+
+function forward(address payable _receipt) public payable {
+
+_receipt.transfer(msg.value);
+}
+}
+
+{
+  "optimizer": {
+    "enabled": true,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "libraries": {}
+}
---
+++ contracts_bscscan/testnet/f6/f61973254c399713efc7c303c9f150be75829798_Forwarder.sol
@@ -0,0 +1,26 @@
+pragma solidity 0.4.21;
+
+contract Forwarder{
+
+function forward(address _receipt) public payable {
+
+_receipt.transfer(msg.value);
+}
+}
+
+{
+  "optimizer": {
+    "enabled": true,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "libraries": {}
+}
