cbbd5f9d8a418ee5987ce1a3c7a1bd6075b0525e
minion <minion>
2021-09-15 06:08:38 +0000

    with 💕

---
+++ contracts_polygonscan/mumbai/2b/2B2EA0600f501CeD9b262d66D82f1474882fd0d5_CTokenChiMock.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.0;
+import "./ISourceMock.sol";
+
+
+contract CTokenChiMock is ISourceMock {
+    uint public exchangeRateStored;
+    uint counter;
+
+    function set(uint chi) external override {
+        exchangeRateStored = chi;
+    }
+
+    function exchangeRateCurrent() public returns (uint) {
+        counter++;
+        return exchangeRateStored;
+    }
+}
+
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.0;
+
+interface ISourceMock {
+    function set(uint) external;
+}
+
+{
+  "optimizer": {
+    "enabled": true,
+    "runs": 1500
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "metadata": {
+    "useLiteralContent": true
+  },
+  "libraries": {}
+}
