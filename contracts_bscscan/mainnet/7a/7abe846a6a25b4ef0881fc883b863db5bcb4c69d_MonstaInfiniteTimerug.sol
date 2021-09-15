28c6e73d2f31d673646b694b237e9012ba0d943d
minion <minion>
2021-09-15 21:07:55 +0000

    with 💕

---
+++ contracts_bscscan/mainnet/7a/7abe846a6a25b4ef0881fc883b863db5bcb4c69d_MonstaInfiniteTimerug.sol
@@ -0,0 +1,71 @@
+/**
+ *Submitted for verification at BscScan.com on 2021-09-15
+*/
+
+/**
+No fees. No marketing. No telegram. 60 lines of code and something for you monkeys to play with while
+we all get fucking time rugged by Monsta Infinite
+
+Burning liquidity and renouncing ownership.
+
+*/
+
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.4;
+ 
+contract MonstaInfiniteTimerug {
+    string public name = "Monsta Infinite Timerug";
+    string public symbol = "MIT";
+    uint256 public totalSupply = 200 * 10 ** 18;
+    uint8 public decimals = 18;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+        );
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    constructor() {
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value)
+        public
+        returns (bool success)
+    {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+}
