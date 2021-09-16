19ba81338f1614ddc2f7f485077d3f3e9f8db6d7
minion <minion>
2021-09-16 07:15:37 +0000

    with 💕

---
+++ contracts_bscscan/testnet/96/96DeEBC0A0415479c7bB492d2AE5Da5CD03A18D9_MockOracle.sol
@@ -0,0 +1,51 @@
+pragma solidity >=0.8.0;
+
+interface IOracle {
+	function update() external;
+
+	function consult(address token, uint256 amountIn)
+		external
+		view
+		returns (uint256 amountOut);
+}
+
+pragma solidity >=0.8.0;
+import '../Interfaces/IOracle.sol';
+
+contract MockOracle is IOracle {
+	function update() public override {}
+
+	function consult(address token, uint256 amountIn)
+		external
+		view
+		override
+		returns (uint256 amountOut)
+	{
+		return price;
+	}
+
+	uint256 private price = 10**18;
+
+	function setPrice(uint256 price_) external {
+		price = price_;
+	}
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": true,
+    "runs": 200
+  },
+  "evmVersion": "istanbul",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
