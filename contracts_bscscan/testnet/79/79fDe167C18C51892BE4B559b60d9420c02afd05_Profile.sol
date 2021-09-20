2501add497759c235fa1380c21cdd5e01115c428
minion <minion>
2021-09-20 06:54:49 +0000

    with 💕

---
+++ contracts_bscscan/testnet/79/79fDe167C18C51892BE4B559b60d9420c02afd05_Profile.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Profile {
+    event save(address user, string data);
+    mapping(address => string) public profiles;
+    mapping(address => string[]) public playersHistory;
+    
+    function saveProfile (string calldata _data) external{
+        profiles[msg.sender] = _data;
+        emit save(msg.sender,_data);
+    }
+    function updatePlayers (string calldata _data) external{
+        playersHistory[msg.sender].push(_data);
+    }
+    function getPlayersHistory (
+        address _address
+    )
+    external view returns (string[] memory)
+    {
+        string[] memory tempList = playersHistory[_address];
+        return tempList;
+    }
+}
+
+{
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "libraries": {}
+}
---
+++ contracts_bscscan/testnet/d2/d2c8A42966172ceeBe61605138640AE1c17C9380_Test.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+pragma experimental ABIEncoderV2;
+
+contract Test {
+    function test() view external returns(uint[] memory value) {
+        value = new uint[](10);
+        value[0] = 1;
+        value[1] = 2;
+    }
+}
+
+{
+  "optimizer": {
+    "enabled": true,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "metadata": {
+    "useLiteralContent": true
+  },
+  "libraries": {}
+}
