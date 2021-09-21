88d09bc5ad2868c2a82d400a16438302a01d2eea
minion <minion>
2021-09-21 16:20:47 +0000

    with 💕

---
+++ contracts_polygonscan/mumbai/d7/d7e47e0cDA43aE053F62d89FeBEbFC1c931a4c43_StringUtils.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+library StringUtils {
+  function equals(string memory self, string memory b) public pure returns (bool) {
+    return (keccak256(bytes(self)) == keccak256(bytes(b)));
+  }
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": true,
+    "runs": 200
+  },
+  "evmVersion": "london",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
