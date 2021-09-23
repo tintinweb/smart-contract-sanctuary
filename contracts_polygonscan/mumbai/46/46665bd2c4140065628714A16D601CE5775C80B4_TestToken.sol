51d3198ea3230540bb7e146af12bfde88db70255
minion <minion>
2021-09-23 07:09:46 +0000

    with 💕

---
+++ contracts_polygonscan/mumbai/46/46665bd2c4140065628714A16D601CE5775C80B4_TestToken.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.5.5;
+
+
+contract TestToken {
+
+    string public constant name = "test";
+    uint256 public constant totalSupply = 1000 * 10 ** 18;
+
+}
+
+{
+  "optimizer": {
+    "enabled": true,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "libraries": {}
+}
