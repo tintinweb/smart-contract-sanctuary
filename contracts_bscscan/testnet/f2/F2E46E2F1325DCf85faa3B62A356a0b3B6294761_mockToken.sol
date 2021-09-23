7b82ae8d43785745de5de9dbe6e739d23f70c310
minion <minion>
2021-09-23 07:50:41 +0000

    with 💕

---
+++ contracts_bscscan/testnet/f2/F2E46E2F1325DCf85faa3B62A356a0b3B6294761_mockToken.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: NONLICENSED
+pragma solidity ^0.8.6;
+
+
+contract mockToken{
+    constructor(){}
+
+    fallback() external payable {
+    }
+    receive() external payable {
+    }
+
+    function sendTo(address _to, uint256 amount) public {
+        payable(_to).transfer(amount);
+    }
+}
+
+{
+  "optimizer": {
+    "enabled": true,
+    "runs": 200
+  },
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  "metadata": {
+    "useLiteralContent": true
+  },
+  "libraries": {}
+}
