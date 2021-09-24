2183375899e93f6730297c241e39b790cd3e4c6a
minion <minion>
2021-09-24 07:07:38 +0000

    with 💕

---
+++ contracts_polygonscan/mumbai/62/62D76142623615383fC6d9AF6eb064081F0c678E_Oracle.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.8.7;
+
+contract Oracle {
+    address private admin;
+    uint256 public rand;
+
+    constructor() {
+        admin = msg.sender;
+    }
+
+    function feedRandomness(uint256 _rand) external {
+        require(msg.sender == admin);
+        rand = _rand;
+    }
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "evmVersion": "london",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
