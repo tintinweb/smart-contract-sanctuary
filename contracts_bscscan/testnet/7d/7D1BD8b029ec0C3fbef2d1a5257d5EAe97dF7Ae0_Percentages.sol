dac26abd5d0e4f9abd06b717f459816b6db3eaa8
minion <minion>
2021-09-27 07:49:12 +0000

    with 💕

---
+++ contracts_bscscan/testnet/7d/7D1BD8b029ec0C3fbef2d1a5257d5EAe97dF7Ae0_Percentages.sol
@@ -0,0 +1,42 @@
+pragma solidity ^0.8.4;
+
+library Percentages {
+    // Get value of a percent of a number
+    function calcPortionFromBasisPoints(uint _amount, uint _basisPoints) public pure returns(uint) {
+        if(_basisPoints == 0 || _amount == 0) {
+            return 0;
+        } else {
+            uint _portion = _amount * _basisPoints / 10000;
+            return _portion;
+        }
+    }
+
+    // Get basis points (percentage) of _portion relative to _amount
+    function calcBasisPoints(uint _amount, uint  _portion) public pure returns(uint) {
+        if(_portion == 0 || _amount == 0) {
+            return 0;
+        } else {
+            uint _basisPoints = (_portion * 10000) / _amount;
+            return _basisPoints;
+        }
+    }
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": true,
+    "runs": 200
+  },
+  "evmVersion": "istanbul",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
---
+++ contracts_bscscan/testnet/f9/f97374a87dC3C9E9B4549aBB07a38F1dD677c378_Migration.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract Migration {
+  address public owner = msg.sender;
+  address public instance;
+
+  modifier restricted() {
+    require(msg.sender == owner, "Owner only");
+    _;
+  }
+
+  function setCompleted(address _instance) public restricted {
+    instance = _instance;
+  }
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "evmVersion": "byzantium",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
