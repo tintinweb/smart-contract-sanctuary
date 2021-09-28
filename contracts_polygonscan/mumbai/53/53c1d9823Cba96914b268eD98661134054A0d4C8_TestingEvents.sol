96516293a9c95dba27b496040069399377eeef29
minion <minion>
2021-09-28 07:08:36 +0000

    with 💕

---
+++ contracts_polygonscan/mumbai/53/53c1d9823Cba96914b268eD98661134054A0d4C8_TestingEvents.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract TestingEvents {
+  event SignUpForRace(address indexed racer, uint256 indexed car, uint256[] modules);
+
+  constructor() {}
+
+  function sendEvent(uint256 car, uint256[] memory modules) public {
+    emit SignUpForRace(msg.sender, car, modules);
+  }
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "evmVersion": "london",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
