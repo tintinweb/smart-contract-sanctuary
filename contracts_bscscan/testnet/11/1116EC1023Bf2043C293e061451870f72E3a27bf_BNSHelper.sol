b5307769100ea76e89cb9f7f8ffdf36205947f3d
minion <minion>
2021-10-01 07:50:10 +0000

    with 💕

---
+++ contracts_bscscan/testnet/11/1116EC1023Bf2043C293e061451870f72E3a27bf_BNSHelper.sol
@@ -0,0 +1,29 @@
+pragma solidity >=0.5.0;
+
+contract BNSHelper {
+    function checkTokenId(string memory name) pure public returns(uint256) {
+        bytes32 label = keccak256(bytes(name));
+        uint256 tokenId = uint256(label);
+
+        return tokenId;
+    }
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": false,
+    "runs": 200
+  },
+  "evmVersion": "istanbul",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
