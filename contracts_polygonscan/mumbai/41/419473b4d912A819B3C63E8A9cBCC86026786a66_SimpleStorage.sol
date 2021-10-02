4f5883e3d18cc0790f580315bb8e33dc35f08b2d
minion <minion>
2021-10-02 07:08:08 +0000

    with 💕

---
+++ contracts_polygonscan/mumbai/41/419473b4d912A819B3C63E8A9cBCC86026786a66_SimpleStorage.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.9;
+
+contract SimpleStorage {
+    uint256 storedData;
+
+    function set(uint256 x) public {
+        storedData = x;
+    }
+
+    function get() public view returns (uint256) {
+        return storedData;
+    }
+}
+
+{
+  "remappings": [],
+  "optimizer": {
+    "enabled": true,
+    "runs": 200
+  },
+  "evmVersion": "byzantium",
+  "libraries": {},
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  }
+}
