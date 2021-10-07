01545cbaa3abe24e6e33de76271173f19559db3d
minion <minion>
2021-10-07 22:41:55 +0000

    with 💕

---
+++ contracts_arbiscan/mainnet/04/04aa9e9cbf5fe828d775ef405fd1d8d5c9e0527a_PlaceHolder.sol
@@ -0,0 +1,12 @@
+/**
+ *Submitted for verification at arbiscan.io on 2021-09-22
+*/
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+pragma experimental ABIEncoderV2;
+
+contract PlaceHolder {
+    
+}
