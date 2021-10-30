0e9d6f670d02f3c4f254b74f4192288f4efce2c4
minion <minion>
2021-10-30 07:54:44 +0000

    with 💕

---
+++ contracts_bscscan/testnet/18/182d5d53553e5F387c5BC066f6F0b71DDC056B83_test.sol
@@ -0,0 +1,16 @@
+/**
+ *Submitted for verification at BscScan.com on 2021-10-29
+*/
+
+pragma solidity 0.6.12;
+pragma experimental ABIEncoderV2;
+
+contract test {
+    
+    uint256 result;
+    
+    
+    function deposit(uint256 a, uint256 b) public {
+        
+    }
+}
