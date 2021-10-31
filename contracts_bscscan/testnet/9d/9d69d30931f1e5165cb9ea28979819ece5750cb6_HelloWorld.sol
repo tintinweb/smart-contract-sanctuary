fd5ab1514719685c766bd93d91a8ece412cfbb94
minion <minion>
2021-10-31 07:54:02 +0000

    with 💕

---
+++ contracts_bscscan/testnet/9d/9d69d30931f1e5165cb9ea28979819ece5750cb6_HelloWorld.sol
@@ -0,0 +1,20 @@
+/**
+ *Submitted for verification at BscScan.com on 2021-10-30
+*/
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+
+contract HelloWorld {
+    
+    
+    function renderHelloWorld() public pure returns (string memory) {
+        
+        
+            return 'hello'; 
+        
+        
+    }
+    
+}
