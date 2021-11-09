90fa659848d756c573ea382ccc60a82e4a26b7f2
minion <minion>
2021-11-09 21:07:40 +0000

    with 💕

---
+++ contracts_arbiscan/mainnet/95/95f4538C3950CE0EF5821f2049aE2aC5cCade68D_Forwarder.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.0;
+
+contract Forwarder {
+    function checker(bytes memory execData)
+        external
+        pure
+        returns (bool, bytes memory)
+    {
+        return (true, execData);
+    }
+}
