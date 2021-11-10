f88e8f592c06fbf6952f1d30fce3f7c15a923099
minion <minion>
2021-11-10 07:11:41 +0000

    with 💕

---
+++ contracts_polygonscan/mumbai/7b/7B5a2E5Aa940ADD52d19738102dd0f45C0f3572B_SmartContract.sol
@@ -0,0 +1,13 @@
+/**
+ *Submitted for verification at polygonscan.com on 2021-11-10
+*/
+
+// SPDX-License-Identifier: SPDX-License-Identifier
+
+pragma solidity ^0.8.0;
+
+
+// 
+contract SmartContract {
+  string public name = "Smart Contract";
+}
