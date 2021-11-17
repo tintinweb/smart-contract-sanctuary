8b74fdbb6cacfa3513aac6d5d1afbc3995050df8
minion <minion>
2021-11-17 06:16:29 +0000

    with 💕

---
+++ contracts_polygonscan/mumbai/b6/b688725AB5A09735a6335E94CEF456eD38570b2a_Test.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+// this is a contract for testing purpose only
+contract Test {
+
+    function getData() public pure returns (string memory) {
+        return "testing data";
+    }
+
+}
