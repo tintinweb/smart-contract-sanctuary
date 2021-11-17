1efde3253051421ea1aabe689a78daba27b902c9
minion <minion>
2021-11-17 06:58:13 +0000

    with 💕

---
+++ contracts_bscscan/testnet/2f/2f5e145c1e83f262ea85f26af6dc192cdf1567c4_HelloWorld.sol
@@ -0,0 +1,10 @@
+/**
+ *Submitted for verification at BscScan.com on 2021-11-16
+*/
+
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.7;
+
+contract HelloWorld {
+    string public greetings = "You ye niubi!";
+}
