9e90198f2eb8dbed5ef6e13e63b636b1065689fe
minion <minion>
2021-11-30 07:42:06 +0000

    with 💕

---
+++ contracts_bscscan/testnet/5a/5a6dc7b254da70aab815c3f1ad97ae5d8ef55116_KawaiiCheckPermitDoubleSpendingBatch.sol
@@ -0,0 +1,17 @@
+/**
+ *Submitted for verification at BscScan.com on 2021-11-30
+*/
+
+pragma solidity 0.6.12;
+
+interface Signed {
+  function checkPermitDoubleSpendingBatch(bytes32[] memory _datas) external view returns (bool[] memory);
+}
+
+contract KawaiiCheckPermitDoubleSpendingBatch {
+  function checkPermitDoubleSpendingBatchOfSignedContract(Signed _signedContract, bytes32[] memory _data) external view returns (bool[] memory) {
+    bool[] memory res = new bool[](_data.length);
+    res =  Signed(_signedContract).checkPermitDoubleSpendingBatch(_data);
+    return res;
+  }
+}
---
+++ contracts_bscscan/testnet/91/91f54a792349ba4b3c226daaf02841b85062f9ed_HelloWorld.sol
@@ -0,0 +1,11 @@
+/**
+ *Submitted for verification at BscScan.com on 2021-11-29
+*/
+
+// SPDX-License-Identifier: MIT
+// compiler version must be greater than or equal to 0.8.3 and less than 0.9.0
+pragma solidity ^0.8.3;
+
+contract HelloWorld {
+    string public greet = "Hello World!";
+}
