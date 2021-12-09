671a74fbbd89f5d194a4cd9dafd6e8d596cc8083
minion <minion>
2021-12-09 06:34:15 +0000

    with 💕

---
+++ contracts_polygonscan/mumbai/aa/aa8C69A56adBA756Aa1431A8A6348724e41Af33d_MyContract.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.9;
+
+contract MyContract {
+    function sayHello() public pure returns (string memory) {
+        return "Hello World!";
+    }
+}
