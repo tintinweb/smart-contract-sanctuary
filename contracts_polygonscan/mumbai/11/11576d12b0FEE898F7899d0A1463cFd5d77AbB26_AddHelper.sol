8a593aa932c631539c3cf828109188969b785f02
minion <minion>
2021-12-10 06:35:05 +0000

    with 💕

---
+++ contracts_polygonscan/mumbai/11/11576d12b0FEE898F7899d0A1463cFd5d77AbB26_AddHelper.sol
@@ -0,0 +1,13 @@
+/**
+ *Submitted for verification at polygonscan.com on 2021-12-09
+*/
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.0;
+
+contract AddHelper {
+    function add(uint256 a, uint256 b) public pure returns (uint256) {
+  return a + b;
+    }
+}
---
+++ contracts_polygonscan/mumbai/d0/d078aCb4433De76eACdFAbEaE7aD7c6d316a369D_AddHelper.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.0;
+
+contract AddHelper {
+    function add(uint256 a, uint256 b) public pure returns (uint256) {
+  return a + b;
+    }
+}
