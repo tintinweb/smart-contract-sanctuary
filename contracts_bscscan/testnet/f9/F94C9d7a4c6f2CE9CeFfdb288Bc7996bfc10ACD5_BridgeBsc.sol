5ff990cab2526f2534addc101595083e1b9b05da
minion <minion>
2021-12-12 07:17:04 +0000

    with 💕

---
+++ contracts_bscscan/testnet/f9/F94C9d7a4c6f2CE9CeFfdb288Bc7996bfc10ACD5_BridgeBsc.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import './BridgeBase.sol';
+
+contract BridgeBsc is BridgeBase {
+    constructor(address token) BridgeBase (token){
+    }
+}
