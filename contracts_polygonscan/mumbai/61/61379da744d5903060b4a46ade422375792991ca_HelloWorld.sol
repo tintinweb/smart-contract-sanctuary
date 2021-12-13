6c7b4a431f93bd37181e5629f624233b98dfef30
minion <minion>
2021-12-13 06:35:12 +0000

    with 💕

---
+++ contracts_polygonscan/mumbai/61/61379da744d5903060b4a46ade422375792991ca_HelloWorld.sol
@@ -0,0 +1,10 @@
+/**
+ *Submitted for verification at polygonscan.com on 2021-12-12
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0;
+
+contract HelloWorld {
+    string public greet = "Hello World!";
+}
