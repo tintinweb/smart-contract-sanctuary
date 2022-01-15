832eec25a2e6359b39e38236f2bb439d64f6205a
minion <minion>
2022-01-15 07:50:54 +0000

    with 💕

---
+++ contracts_snowtrace/testnet/aa/aa600f368890087425a62f5dfa1e4346c40e15ca_WAVAX.sol
@@ -0,0 +1,18 @@
+/**
+ *Submitted for verification at testnet.snowtrace.io on 2022-01-14
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+contract WAVAX{
+    uint public storedData;
+
+    constructor(){
+        storedData = 0;
+    }
+
+    function set(uint setValue) external {
+        storedData = setValue;
+    }
+}
